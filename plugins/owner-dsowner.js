1b31ce6b59bb3bf0eb53d48c1acf80570dbb7b14
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-24 14:39:39 -0400

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -1,4 +1,4 @@
-/* Codigo hecho por @Fabri115 y mejorado por ianalejandro */
+/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
 
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
 import path from 'path'
@@ -6,17 +6,17 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m, fake);
 }
-await conn.reply(m.chat, '🚩 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, rcanal, )
-m.react('❄️')
+await conn.reply(m.chat, `${emoji2} Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...`, m, fake);
+m.react(rwait)
 
-let sessionPath = './BarbozaJadiBot/'
+let sessionPath = `./${sessions}/`
 
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, '🚩 *La carpeta está vacía*', m, rcanal, )
+return await conn.reply(m.chat, `${emoji} La carpeta está vacía.`, m, fake);
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -27,23 +27,22 @@ filesDeleted++;
 }
 }
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🚩 *La carpeta esta vacía*',  m, rcanal, )
+await conn.reply(m.chat, `${emoji2} La carpeta esta vacía.`, m, fake);
 } else {
-m.react('✅')
-await conn.reply(m.chat, `🚩 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
-conn.reply(m.chat, `🚩 *¡Hola! ¿logras verme?*`, m, rcanal, )
+m.react(done)
+await conn.reply(m.chat, `${emoji} Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json.`, m, fake);
+conn.reply(m.chat, `${emoji} *¡Hola! ¿logras verme?*`, m, fake);
 
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err);
-await conn.reply(m.chat, '🚩 *Ocurrió un fallo*',  m, rcanal, )
+await conn.reply(m.chat, `${msm} Ocurrió un fallo.`, m, fake);
 }
 
 }
 handler.help = ['dsowner']
-handler.tags = ['fix', 'owner']
-handler.command = /^(delzero|dsowner|clearallsession)$/i
-
-handler.rowner = true
+handler.tags = ['owner']
+handler.command = ['delai', 'dsowner', 'clearallsession']
+handler.rowner = true;
 
 export default handler
