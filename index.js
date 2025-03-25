1635a961e665181d7bab307996d773ff0bf47727
deylinqff <maycraftminecraft4@gmail.com>
2025-03-24 23:52:47 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 //let require = createRequire(megu)
 let { say } = cfonts
 
-console.log(chalk.bold.redBright(`\n✰ Iniciando kirit-Bot-MD ✰\n`))
+console.log(chalk.bold.redBright(`\n✰ Iniciando Astro-Bot ✰\n`))
 
-say('kirit-Bot-MD', {
+say('Astro-Bot', {
 font: 'block',
 align: 'center',
 colors: ['magentaBright']
