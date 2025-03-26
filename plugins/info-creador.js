cd4d860702a5b221348c8ad822d6bc3be0475d19
BrayanOFC <lourdesagueda93@gmail.com>
2025-03-26 11:30:40 -0700

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,59 +1,38 @@
+// Código creado por Deylin
+// https://github.com/deylinqff
+// No quites créditos
+
 import PhoneNumber from 'awesome-phonenumber';
 
-let handler = async (m, { conn }) => {
-  m.react('⚡');
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  let pp = await conn.profilePictureUrl(who).catch(_ => 'https://qu.ax/PRgfc.jpg');
-  let biografia = await conn.fetchStatus(`${suittag}@s.whatsapp.net`).catch(_ => 'Sin Biografía');
-  let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}@s.whatsapp.net`).catch(_ => 'Sin Biografía');
-  let bio = biografia.status?.toString() || 'Sin Biografía';
-  let biobot = biografiaBot.status?.toString() || 'Sin Biografía';
-  let name = await conn.getName(who);
+async function handler(m, { conn }) {
+  m.react('👑');
 
-  await sendContactArray(conn, m.chat, [
-    [`${suittag}`, `ᰔᩚ creador1`, creador, `❀ No Hacer Spam`, correo, `⊹˚• honduras •˚⊹`, XD, bio],
-    [`${conn.user.jid.split('@')[0]}`, `✦ ᴇs ᴜɴ ᴄʀᴇᴀᴅᴏʀ ᴅᴇ ʙᴏᴛs`, ʙʀᴀʏᴀɴ, dev, correo, `ᴍᴇxɪᴄᴏ`, channel, biocreador]
-  ], m);
+  const creadores = [
+    { numer: '526641804242', nombre: 'Brayan', descripcion: 'Creador de bots y desarrollador' },
+    { numer: '50488198573', nombre: 'Deylin', descripcion: 'Experto en tecnología y automatización' }
+  ];
+
+  for (const creador of creadores) {
+    const vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${creador.nombre};;;
+FN:${creador.nombre}
+TEL;waid=${creador.numero}:${new PhoneNumber('+' + creador.numero).getNumber('international')}
+NOTE:${creador.descripcion}
+END:VCARD
+    `.trim();
+
+    await conn.sendMessage(
+      m.chat,
+      { contacts: { displayName: creador.nombre, contacts: [{ vcard }] } },
+      { quoted: m }
+    );
+  }
 }
 
-handler.help = ["creador", "owner"];
-handler.tags = ["info"];
+handler.help = ['owner'];
+handler.tags = ['main'];
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
 export default handler;
-
-async function sendContactArray(conn, jid, data, quoted, options) {
-  if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data];
-  let contacts = [];
-  for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
-    number = number.replace(/[^0-9]/g, '');
-    let njid = number + '@s.whatsapp.net';
-    let vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-item.ORG:${isi}
-item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
-item1.X-ABLabel:${isi1}
-item2.EMAIL;type=INTERNET:${isi2}
-item2.X-ABLabel:Email
-item3.ADR:;;${isi3};;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:Region
-item4.URL:${isi4}
-item4.X-ABLabel:Website
-item5.X-ABLabel:${isi5}
-END:VCARD`.trim();
-    contacts.push({ vcard, displayName: name });
-  }
-  return await conn.sendMessage(jid, {
-    contacts: {
-      displayName: (contacts.length > 1 ? `Contactos` : contacts[0].displayName) || null,
-      contacts,
-    }
-  }, {
-    quoted,
-    ...options
-  });
-}
