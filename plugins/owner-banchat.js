18224c00d4ff4a3d1764794e07d3371ac9ba042c
deylinqff <maycraftminecraft4@gmail.com>
2025-03-25 23:49:46 -0600

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,42 +1,12 @@
-/*let handler = async (m, { conn, usedPrefix, command, args }) => {
-  if (!(m.chat in global.db.data.chats)) 
-    return conn.reply(m.chat, '🔴 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake);
+let handler = async (m) => {
 
-  let chat = global.db.data.chats[m.chat];
+global.db.data.chats[m.chat].isBanned = true
+conn.reply(m.chat, `《★》𝗘𝗹 𝗕𝗼𝘁 𝗛𝗮 𝗦𝗶𝗱𝗼 𝗗𝗲𝘀𝗮𝗰𝘁𝗶𝘃𝗮𝗱𝗼 𝗘𝗻 𝗘𝘀𝘁𝗲 𝗖𝗵𝗮𝘁`, m, rcanal)
 
-  if (command === 'kirito') {
-    if (args.length === 0) {
-      const estado = chat.isBanned ? '⚠️ *DESACTIVADO*' : '✅ *ACTIVADO*';
-      const info = `👑 *KIRITO-BOT CONTROL*  
-╭━━━━━━━━━━━━━╮  
-┃ *🔥 COMANDOS DISPONIBLES:*  
-┃ ✦ *${usedPrefix}kirito on* – ⚡ 𝗔𝗰𝘁𝗶𝘃𝗮𝗿  
-┃ ✦ *${usedPrefix}kirito off* – ⚡ 𝗗𝗲𝘀𝗮𝗰𝘁𝗶𝘃𝗮𝗿  
-╰━━━━━━━━━━━━━╯  
-🌟 *Estado actual:* ${estado}`;
+}
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^banchat$/i
+handler.prems = true 
 
-      return conn.reply(m.chat, info, m, fake);
-    }
-
-    if (args[0] === 'off') {
-      if (chat.isBanned) 
-        return conn.reply(m.chat, '⭕ *¡KIRITO-BOT YA ESTABA DESACTIVADO!*', m, fake);
-
-      chat.isBanned = true;
-      return conn.reply(m.chat, '⚠️ *¡KIRITO-BOT HA SIDO DESACTIVADO EN ESTE CHAT!*', m, fake);
-    } else if (args[0] === 'on') {
-      if (!chat.isBanned) 
-        return conn.reply(m.chat, '⭕ *¡KIRITO-BOT YA ESTABA ACTIVADO!*', m, fake);
-
-      chat.isBanned = false;
-      return conn.reply(m.chat, '✅ *¡KIRITO-BOT HA SIDO ACTIVADO EN ESTE CHAT!*', m, fake);
-    }
-  }
-};
-
-handler.help = ['kirito'];
-handler.tags = ['grupo'];
-handler.command = ['kirito'];
-handler.admin = true;
-
-export default handler;*/
+export default handler
