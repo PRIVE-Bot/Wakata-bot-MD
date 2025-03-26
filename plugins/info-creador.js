e524b597557d2e7bbae853d94f792ffaf74ceaf9
deylinqff <maycraftminecraft4@gmail.com>
2025-03-26 11:49:20 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -8,8 +8,8 @@ async function handler(m, { conn }) {
   m.react('👑');
 
   const creadores = [
-    { numero: '50488198573', nombre: 'Deylin', descripcion: 'Creador de bots y desarrollador' },
-    { numero: '+52 663 390 0512', nombre: 'Brayan', descripcion: 'Experto en tecnología y automatización' }
+    { numero: '526633900512', nombre: 'Brayan', descripcion: 'Creador de bots y desarrollador' },
+    { numero: '50488198573', nombre: 'Deylin', descripcion: 'Experto en tecnología y automatización' }
   ];
 
   const contactos = creadores.map(creador => ({
