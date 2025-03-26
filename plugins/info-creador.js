d15ab7f160b5c2ed46d4387d09df7ebebd81c308
deylinqff <maycraftminecraft4@gmail.com>
2025-03-26 12:50:54 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,38 +1,42 @@
-// Código creado por Deylin
+// código creado por Deylin 
 // https://github.com/deylinqff
-// No quites créditos
+// no quites créditos 
 
 import PhoneNumber from 'awesome-phonenumber';
 
 async function handler(m, { conn }) {
-  m.react('👑');
+    const creadores = [
+        { numero: '526641804242', nombre: 'Brayan' },
+        { numero: '50488198573', nombre: 'Deylin' }
+    ];
 
-  const creadores = [
-    { numero: '+526641804242', nombre: 'Brayan', descripcion: 'Creador de bots y desarrollador' },
-    { numero: '50488198573', nombre: 'Deylin', descripcion: 'Experto en tecnología y automatización' }
-  ];
+    for (const creador of creadores) {
+        let numcreador = creador.numero;
+        let ownerJid = numcreador + '@s.whatsapp.net';
 
-  for (const creador of creadores) {
-    const vcard = `
+        let name = await conn.getName(ownerJid) || creador.nombre;
+        let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
+
+        let vcard = `
 BEGIN:VCARD
 VERSION:3.0
-N:;${creador.nombre};;;
-FN:${creador.nombre}
-TEL;waid=${creador.numero}:${new PhoneNumber('+' + creador.numero).getNumber('international')}
-NOTE:${creador.descripcion}
-END:VCARD
-    `.trim();
+N:;${name};;;
+FN:${name}
+TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+NOTE:${about}
+END:VCARD`.trim();
 
-    await conn.sendMessage(
-      m.chat,
-      { contacts: { displayName: creador.nombre, contacts: [{ vcard }] } },
-      { quoted: m }
-    );
-  }
+        await conn.sendMessage(m.chat, {
+            contacts: {
+                displayName: name,
+                contacts: [{ vcard }]
+            }
+        }, { quoted: m });
+    }
 }
 
-handler.help = ['owner'];
-handler.tags = ['main'];
+handler.help = ['owner']; 
+handler.tags = ['main']; 
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
 export default handler;
