50a4cddda959aeab00e068752c1a7f65306a8b51
deylinqff <maycraftminecraft4@gmail.com>
2025-03-25 19:04:04 -0600

    Actualizar grupo-tag.js

--- plugins/grupo-tag.js
+++ plugins/grupo-tag.js
@@ -69,6 +69,6 @@ handler.tags = ['grupo']
 handler.command = ['hidetag', 'notificar', 'notify', 'tag']
 handler.group = true
 handler.admin = true
-handler.register = true
+
 
 export default handler
