d74dfa4f094c7a458758fc4b1a978504cce2bcf2
deylinqff <maycraftminecraft4@gmail.com>
2025-03-26 10:54:30 -0600

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '〽️l🔥 *¡Este chat no está registrado!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '👑 *¡𝚅𝚎𝚐𝚎𝚝𝚊-𝙱𝚘𝚝-𝙼𝙱 ɴᴏ ᴇsᴛᴀ ʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '👑 *¡ᴀsᴛʀᴏ-ʙᴏᴛ ɴᴏ ᴇsᴛᴀ ʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '⚡ *¡𝚅𝚎𝚐𝚎𝚝𝚊-𝙱𝚘𝚝-𝙼𝙱 ʏᴀ ғᴜᴇ ʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
+await conn.reply(m.chat, '⚡ *¡ᴀsᴛʀᴏ-ʙᴏᴛ ʏᴀ ғᴜᴇ ʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['grupo'];
