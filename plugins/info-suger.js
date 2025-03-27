d16e2616f55170e384ef7ba506baaed9265ff3e6
deylinqff <maycraftminecraft4@gmail.com>
2025-03-26 23:53:54 -0600

    Actualizar info-suger.js

--- plugins/info-suger.js
+++ plugins/info-suger.js
@@ -1,4 +1,4 @@
-// Comando para sugerir nuevos comandos
+
 let sugerirHandler = async (m, { conn, text, usedPrefix }) => {
   if (!text) {
     return conn.reply(m.chat, `❗️ Por favor, ingrese su sugerencia en el siguiente formato:\n\ncomando | descripción\n\nEjemplo:\n!saludo | Envía un mensaje de bienvenida al usuario.`, m)
@@ -26,8 +26,8 @@ let sugerirHandler = async (m, { conn, text, usedPrefix }) => {
 
 _Para aprobar o rechazar la sugerencia, el staff debe responder a este mensaje con .aceptar o .noaceptar seguido de una razón (opcional)._`
 
-  // Envía la sugerencia al grupo de staff y/o al creador
-  // Reemplaza 'STAFF_GROUP_ID' y 'CREADOR_ID@s.whatsapp.net' por los IDs correspondientes.
+  
+ correspondientes.
   await conn.reply('CREADOR_ID@s.whatsapp.net', m.quoted ? teks + '\n\n' + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
   await conn.reply('STAFF_GROUP_ID', m.quoted ? teks + '\n\n' + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
