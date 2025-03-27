57ad9cadf5c714c62551b377a1c3ca6954f65013
deylinqff <maycraftminecraft4@gmail.com>
2025-03-27 15:43:23 -0600

    Actualizar _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -78,7 +78,7 @@ result = await luminsesi(query, username, syms1)
 }
 
 if (result && result.trim().length > 0) {
-await this.reply(m.chat, result, m)
+await this.reply(m.chat, result, m, fake);
 } else {    
 }}}
 return true
