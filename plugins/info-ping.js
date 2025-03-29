96f02ee6974446a3d420a7e095614c32479e6c6e
deylinqff <maycraftminecraft4@gmail.com>
2025-03-28 21:22:31 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -2,17 +2,19 @@ import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
-         let timestamp = speed();
-         let latensi = speed() - timestamp;
-         exec(`neofetch --stdout`, (error, stdout, stderr) => {
-          let child = stdout.toString("utf-8");
-          let ssd = child.replace(/Memory:/, "Ram:");
+    let timestamp = speed();
+    let latensi = speed() - timestamp;
+    
+    exec(`neofetch --stdout`, (error, stdout, stderr) => {
+        let child = stdout.toString("utf-8");
+        let ssd = child.replace(/Memory:/, "Ram:");
 
-          conn.reply(m.chat, `*¡Pong!*\n> ╭─────────────╮\n> │ *Tiempo:* ${latensi.toFixed(4)}ms\n> ╰─────────────╯`, m);
-            });
+        conn.reply(m.chat, `*¡Pong!*\n> ╭─────────────╮\n> │ *Tiempo:* ${latensi.toFixed(4)}ms\n> ╰─────────────╯`, m);
+    });
 }
-handler.help = ['ping']
-handler.tags = ['info']
-handler.command = ['ping', 'p']
+
+// Permitir comandos sin prefijos
+handler.customPrefix = /^(ping|p)$/i
+handler.command = new RegExp
 
 export default handler
