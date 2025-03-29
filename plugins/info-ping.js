021287f8da0e9d8bef9dff3cc9f109360adc5760
deylinqff <maycraftminecraft4@gmail.com>
2025-03-28 21:34:12 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,5 +1,5 @@
 import speed from 'performance-now'
-import { spawn, exec, execSync } from 'child_process'
+import { exec } from 'child_process'
 
 let handler = async (m, { conn }) => {
     let timestamp = speed();
@@ -7,14 +7,13 @@ let handler = async (m, { conn }) => {
     
     exec(`neofetch --stdout`, (error, stdout, stderr) => {
         let child = stdout.toString("utf-8");
-        let ssd = child.replace(/Memory:/, "Ram:");
 
         conn.reply(m.chat, `*¡Pong!*\n> ╭─────────────╮\n> │ *Tiempo:* ${latensi.toFixed(4)}ms\n> ╰─────────────╯`, m);
     });
 }
 
-// Permitir comandos sin prefijos
+
 handler.customPrefix = /^(ping|p)$/i
-handler.command = new RegExp
+handler.command = ['ping', 'p']
 
 export default handler
