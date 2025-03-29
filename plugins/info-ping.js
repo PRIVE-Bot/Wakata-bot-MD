b6db55b4fbfc3e6f244d683e8d8d2ecfac3864d7
deylinqff <maycraftminecraft4@gmail.com>
2025-03-28 21:43:34 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,19 +1,20 @@
 import speed from 'performance-now'
 import { exec } from 'child_process'
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
     let timestamp = speed();
     let latensi = speed() - timestamp;
     
     exec(`neofetch --stdout`, (error, stdout, stderr) => {
         let child = stdout.toString("utf-8");
 
-        conn.reply(m.chat, `*¡Pong!*\n> ╭─────────────╮\n> │ *Tiempo:* ${latensi.toFixed(4)}ms\n> ╰─────────────╯`, m);
+        conn.reply(m.chat, `*¡Pong!*\n> ╭─────────────╮\n> │ *Tiempo:* ${latensi.toFixed(4)}ms\n> ╰─────────────╯`, m, fake);
     });
 }
 
-
-handler.customPrefix = /^(ping|p)$/i
+// Detecta con y sin prefijo
 handler.command = ['ping', 'p']
+handler.customPrefix = /^(ping|p)$/i
+handler.check = (m) => handler.command.includes(m.text.toLowerCase()) || handler.customPrefix.test(m.text)
 
 export default handler
