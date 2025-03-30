d4f7aa08c993df4fa6eb3d84d33c52982ca3a1d3
deylinqff <maycraftminecraft4@gmail.com>
2025-03-29 18:36:02 -0600

    Crear _antisubbot.js

---
+++ plugins/_antisubbot.js
@@ -0,0 +1,28 @@
+import { areJidsSameUser } from '@whiskeysockets/baileys'
+export async function before(m, { participants, conn }) {
+    if (m.isGroup) {
+        let chat = global.db.data.chats[m.chat];
+
+         if (!chat.antiBot2) {
+            return
+        }
+
+
+        let botJid = global.conn.user.jid // JID del bot principal
+
+       if (botJid === conn.user.jid) {
+           return
+        } else {
+           let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id))
+
+          if (isBotPresent) {
+                setTimeout(async () => {
+                    await conn.reply(m.chat, `.  > ✦ 𝐊𝐈𝐑𝐈𝐓𝐎-𝐁𝐎𝐓 ✦ 
+🚀 En esta nave ya está el capitán. La tripulación debe embarcarse en nuevas misiones y explorar universos desconocidos.
+`, m, fake);
+                    await this.groupLeave(m.chat)
+                }, 5000)// 5 segundos
+            }
+        }
+    }
+}
