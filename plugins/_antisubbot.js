f6d3f5b40032000cbb865baae2aff5911c2e101a
deylinqff <maycraftminecraft4@gmail.com>
2025-03-29 18:39:16 -0600

    Actualizar _antisubbot.js

--- plugins/_antisubbot.js
+++ plugins/_antisubbot.js
@@ -17,7 +17,7 @@ export async function before(m, { participants, conn }) {
 
           if (isBotPresent) {
                 setTimeout(async () => {
-                    await conn.reply(m.chat, '> ✦ 𝐊𝐈𝐑𝐈𝐓𝐎-𝐁𝐎𝐓 ✦\n 
+                    await conn.reply(m.chat, '\n> ✦ 𝐊𝐈𝐑𝐈𝐓𝐎-𝐁𝐎𝐓 ✦\n 
 🚀 En esta nave ya está el capitán. La tripulación debe embarcarse en nuevas misiones y explorar universos desconocidos.', m, fake);
                     await this.groupLeave(m.chat)
                 }, 5000)// 5 segundos
