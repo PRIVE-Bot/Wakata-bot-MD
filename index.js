eddb08aadb8c987882733afbae5a0159ed6e73e2
Deylin <deylinbo91@gmail.com>
2025-08-02 23:51:37 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Vegeta-Bot ✰');
+console.log('\n Iniciando Naruto-Bot-MD 🌀');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Astro-Bot', {
+say('Naruto Bot', {
 font: 'block',
 align: 'center',
 colors: ['yellow']
