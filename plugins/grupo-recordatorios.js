701e9adfbfc09565ed463f0a11cd89e4d25a781c
Deylin <deylinbo91@gmail.com>
2025-08-03 13:04:44 -0600

    Actualizar grupo-recordatorios.js

--- plugins/grupo-recordatorios.js
+++ plugins/grupo-recordatorios.js
@@ -1,7 +1,29 @@
 let recordatorios = {};
+import fetch from 'node-fetch';
 
-async function handler(m, { args, command }) {
+async function handler(m, { args, command, conn, participants }) {
+    const chatId = m.chat;
+
     
+    const res = await fetch('https://files.catbox.moe/cduhlw.jpg');
+    const thumb2 = Buffer.from(await res.arrayBuffer());
+
+    
+    const fkontak = {
+        key: {
+            participants: "0@s.whatsapp.net",
+            remoteJid: "status@broadcast",
+            fromMe: false,
+            id: "Halo"
+        },
+        message: {
+            locationMessage: {
+                name: ' 𝗥𝗘𝗖𝗢𝗥𝗗𝗔𝗧𝗢𝗥𝗜𝗢',
+                jpegThumbnail: thumb2
+            }
+        },
+        participant: "0@s.whatsapp.net"
+    };
 
     if (command === 'recordatorio') {
         if (args.length < 2) return m.reply('Uso: *!recordatorio [minutos] [mensaje]*');
@@ -10,14 +32,17 @@ async function handler(m, { args, command }) {
         if (isNaN(tiempo) || tiempo <= 0) return m.reply('El tiempo debe ser un número válido en minutos.');
 
         let mensaje = args.slice(1).join(' ');
-        let chatId = m.chat;
 
         if (recordatorios[chatId]) clearTimeout(recordatorios[chatId].timeout);
 
         let contador = 0;
         function enviarRecordatorio() {
             if (contador < 2) {
-                conn.sendMessage(chatId, { text: `> 🔔 *Recordatorio:*\n\n ${mensaje}` });
+                let mencionados = participants.map(u => u.id);
+                conn.sendMessage(chatId, {
+                    text: `🔔 *Recordatorio:*\n\n${mensaje}`,
+                    mentions: mencionados
+                }, { quoted: fkontak });
                 contador++;
                 recordatorios[chatId].timeout = setTimeout(enviarRecordatorio, tiempo * 60000);
             } else {
@@ -30,7 +55,6 @@ async function handler(m, { args, command }) {
     }
 
     if (command === 'cancelarrecordatorio') {
-        let chatId = m.chat;
         if (recordatorios[chatId]) {
             clearTimeout(recordatorios[chatId].timeout);
             delete recordatorios[chatId];
