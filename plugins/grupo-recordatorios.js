9e3dbad351ab8b963f8e76da112d6e46d76eaaf7
Deylin <deylinbo91@gmail.com>
2025-08-04 09:38:36 -0600

    Actualizar grupo-recordatorios.js

--- plugins/grupo-recordatorios.js
+++ plugins/grupo-recordatorios.js
@@ -4,11 +4,9 @@ import fetch from 'node-fetch';
 async function handler(m, { args, command, conn, participants }) {
     const chatId = m.chat;
 
-    
     const res = await fetch('https://files.catbox.moe/nwgsz3.jpg');
     const thumb2 = Buffer.from(await res.arrayBuffer());
 
-    
     const fkontak = {
         key: {
             participants: "0@s.whatsapp.net",
@@ -26,18 +24,20 @@ async function handler(m, { args, command, conn, participants }) {
     };
 
     if (command === 'recordatorio') {
-        if (args.length < 2) return m.reply('Uso: *!recordatorio [minutos] [mensaje]*');
+        if (args.length < 3) return m.reply('Uso: *!recordatorio [minutos] [veces] [mensaje]*');
 
         let tiempo = parseInt(args[0]);
-        if (isNaN(tiempo) || tiempo <= 0) return m.reply('El tiempo debe ser un número válido en minutos.');
+        let repeticiones = parseInt(args[1]);
+        if (isNaN(tiempo) || tiempo <= 0) return m.reply('⛔ El tiempo debe ser un número válido en minutos.');
+        if (isNaN(repeticiones) || repeticiones <= 0) return m.reply('⛔ Las veces deben ser un número válido mayor a 0.');
 
-        let mensaje = args.slice(1).join(' ');
+        let mensaje = args.slice(2).join(' ');
 
         if (recordatorios[chatId]) clearTimeout(recordatorios[chatId].timeout);
 
         let contador = 0;
         function enviarRecordatorio() {
-            if (contador < 2) {
+            if (contador < repeticiones) {
                 let mencionados = participants.map(u => u.id);
                 conn.sendMessage(chatId, {
                     text: `${mensaje}`,
@@ -51,7 +51,7 @@ async function handler(m, { args, command, conn, participants }) {
         }
 
         recordatorios[chatId] = { timeout: setTimeout(enviarRecordatorio, tiempo * 60000) };
-        m.reply(`✅ Recordatorio activado: *"${mensaje}"* cada ${tiempo} minuto(s), se enviará 2 veces.`);
+        m.reply(`✅ Recordatorio activado: *"${mensaje}"* cada ${tiempo} minuto(s), se enviará ${repeticiones} veces.`);
     }
 
     if (command === 'cancelarrecordatorio') {
