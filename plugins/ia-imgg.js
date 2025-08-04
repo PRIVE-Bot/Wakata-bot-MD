e62f5367e8c1b9eba16dc3dbb7ffd511f868f8aa
Deylin <deylinbo91@gmail.com>
2025-08-03 21:43:19 -0600

    Actualizar ia-imgg.js

--- plugins/ia-imgg.js
+++ plugins/ia-imgg.js
@@ -30,7 +30,7 @@ let handler = async (m, { text, conn }) => {
 
 handler.help = ['imagina <prompt>']
 handler.tags = ['ia']
-handler.command = ['imgia', 'imagina']
+handler.command = ['imgia', 'imagina', 'imgg']
 handler.register = true
 
 export default handler
