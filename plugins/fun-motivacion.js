2c7c4d1c7e4bd7012756bffb5d7052e2e74372e8
Deylin <deylinbo91@gmail.com>
2025-08-06 17:13:30 -0600

    Actualizar fun-motivacion.js

--- plugins/fun-motivacion.js
+++ plugins/fun-motivacion.js
@@ -1,20 +1,9 @@
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import { mensajes } from './motivacion.js' 
 
 let handler = async (m, { conn }) => {
   try {
-    const mensajesPath = path.join(__dirname, './motivacion.js')
-    const rawData = fs.readFileSync(mensajesPath, 'utf-8')
-    const data = JSON.parse(rawData)
-    const mensajes = data.mensajes
-
     const mensaje = mensajes[Math.floor(Math.random() * mensajes.length)]
-    await conn.reply(m.chat, `🌟 *Mensaje para ti:*\n\n"${mensaje}"`, m, fake)
-
+    await conn.reply(m.chat, `🌟 *Mensaje para ti:*\n\n"${mensaje}"`, m)
   } catch (e) {
     await conn.reply(m.chat, '⚠️ Ocurrió un error al leer los mensajes.', m)
     console.error(e)
