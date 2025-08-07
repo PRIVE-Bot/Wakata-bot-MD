4470f7492e06eba62fff36d8813395a10ac1efb8
Deylin <deylinbo91@gmail.com>
2025-08-06 22:23:37 -0600

    Actualizar owner-acep.js

--- plugins/owner-acep.js
+++ plugins/owner-acep.js
@@ -3,7 +3,7 @@
 
 let aceptarHandler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
   
-  if (!m.isGroup) return m.reply(`❌ Este comando solo se puede usar en el grupo del staff.`)
+ // if (!m.isGroup) return m.reply(`❌ Este comando solo se puede usar en el grupo del staff.`)
   if (!isOwner) return m.reply(`❌ No tienes permisos para usar este comando.`)
 
   if (!m.quoted) return m.reply(`❗️ Responde al mensaje de sugerencia para aprobarla.`)
