250d3e520484696d30f5f58038ded9d002586263
Deylin <deylinbo91@gmail.com>
2025-08-06 22:35:34 -0600

    Actualizar owner-acep.js

--- plugins/owner-acep.js
+++ plugins/owner-acep.js
@@ -1,10 +1,8 @@
 // © Deylin 
 
 
-let aceptarHandler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
+let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
   
- // if (!m.isGroup) return m.reply(`${emoji} Este comando solo se puede usar en el grupo del staff.`)
-  if (!isOwner) return m.reply(`${emoji} No tienes permisos para usar este comando.`)
 
   if (!m.quoted) return m.reply(`${emoji} Responde al mensaje de sugerencia para aprobarla.`)
   let razon = text.trim() || 'Sin razón especificada.'
@@ -18,11 +16,11 @@ let aceptarHandler = async (m, { conn, text, usedPrefix, command, isOwner }) => 
   let userId = match[1] + "@s.whatsapp.net"
 
   
-  await conn.reply(userId, `${emoji} *¡Tu sugerencia fue ACEPTADA!*\n\n_El staff ha revisado tu propuesta y la ha aprobado._\nRazón: ${razon}`, m)
+  await conn.reply(userId, `${emoji} *¡Tu sugerencia fue ACEPTADA!*\n\n_El staff ha revisado tu propuesta y la ha aprobado._\nRazón: ${razon}`, m, rcanal)
   m.reply(`${emoji} Sugerencia aceptada y notificada al usuario.`)
 }
 
-aceptarHandler.help = ['aceptar']
-aceptarHandler.tags = ['staff']
-aceptarHandler.command = ['aceptar']
-export default aceptarHandler
+handler.help = ['aceptar']
+handler.tags = ['owner']
+handler.command = ['aceptar']
+export default handler
