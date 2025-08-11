12abe44f9908ab6318a557621405bdf44fa5190d
Deylin <deylinbo91@gmail.com>
2025-08-11 15:32:32 -0600

    Actualizar _antisubbot.js

--- plugins/_antisubbot.js
+++ plugins/_antisubbot.js
@@ -1,4 +1,36 @@
-import { areJidsSameUser } from '@whiskeysockets/baileys'
+export async function before(m, { participants, conn }) {
+  if (m.isGroup) {
+    let chat = global.db.data.chats[m.chat];
+
+    if (!chat.antiBot2) {
+      return
+    }
+
+    let botJid = global.conn.user.jid 
+
+    if (botJid === conn.user.jid) {
+      return
+    } else {
+      let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id))
+
+      if (isBotPresent) {
+        setTimeout(async () => {
+          await conn.reply(m.chat, `*🌀 Aviso Importante*
+
+> Ya hay un bot principal activo en el grupo, por lo tanto no responderé comandos para evitar interferencias.
+¡Gracias por su comprensión!
+
+> *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
+> whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F`, m, rcanal)
+          
+        }, 5000) 
+      }
+    }
+  }
+}
+
+
+/*import { areJidsSameUser } from '@whiskeysockets/baileys'
 export async function before(m, { participants, conn }) {
     if (m.isGroup) {
         let chat = global.db.data.chats[m.chat];
@@ -30,4 +62,4 @@ export async function before(m, { participants, conn }) {
             }
         }
     }
-}
+}*/
