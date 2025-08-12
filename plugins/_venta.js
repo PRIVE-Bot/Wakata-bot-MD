cf5af5a27a6d7d229d6d0901de4bef4d200e38e2
Deylin <deylinbo91@gmail.com>
2025-08-12 15:51:13 -0600

    Actualizar _venta.js

--- plugins/_venta.js
+++ plugins/_venta.js
@@ -1,25 +1,6 @@
+
+
 let handler = async (m, { conn }) => {
- // if (m.isGroup) {
- //   return m.reply('Los pagos solo se pueden enviar en chats privados.', { quoted: m })
- // }
-
-  let texto = `Compra *${global.botname}* por $5 para tus grupos y mucho más. ¡Contáctanos para más información!`
-
-  try {
-    await conn.sendPayment(m.sender, '999999999', texto, m)
-    await conn.sendMessage(m.chat, '💸 Mensaje de venta enviado aquí.', { quoted: m })
-  } catch (e) {
-    await conn.sendMessage(m.chat, 'Ocurrió un error al enviar el pago.', { quoted: m })
-  }
-}
-
-handler.tags = ['main']
-handler.command = handler.help = ['buy', 'comprar']
-
-export default handler
-
-
-/*let handler = async (m, { conn }) => {
   let texto = `Compra *${global.botname}* por $5 para tus grupos y mucho más. ¡Contáctanos para más información!`
 
   try {
@@ -33,4 +14,4 @@ export default handler
 handler.tags = ['main']
 handler.command = handler.help = ['buy', 'comprar']
 
-export default handler*/
+export default handler
