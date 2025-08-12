dee3e2b5759d0c7ef844e8f9b2110a82e2b9d0da
Deylin <deylinbo91@gmail.com>
2025-08-12 15:53:43 -0600

    Actualizar _venta.js

--- plugins/_venta.js
+++ plugins/_venta.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   let texto = `🌟 ¡Compra *${global.botname}* por $5! 🌟\n\nDesbloquea funciones premium y úsalo en tus grupos.\n\n¡Contáctame por privado para más información!`
 
   try {
-    await conn.sendPayment(m.chat, '5', texto, m)
+    await conn.sendPayment(m.chat, '000,5', texto, m)
     await conn.sendMessage(m.chat, '💸 Mensaje de venta enviado aquí.', { quoted: m })
   } catch (e) {
     await conn.sendMessage(m.chat, 'Ocurrió un error al enviar el pago.', { quoted: m })
