a7ae509fd039ae5377769eefd995233f748e582e
Deylin <deylinbo91@gmail.com>
2025-08-12 15:42:47 -0600

    Actualizar _venta.js

--- plugins/_venta.js
+++ plugins/_venta.js
@@ -2,12 +2,8 @@ let handler = async (m, { conn }) => {
   let texto = `Compra *${global.botname}* por $5 para tus grupos y mucho más. ¡Contáctanos para más información!`
 
   try {
-    if ((await conn.onWhatsApp(m.chat))[0]?.exists) {
-      await conn.sendPayment(m.chat, '999999999', texto, m)
-      await conn.sendMessage(m.chat, '💸 Mensaje de venta enviado aquí.', { quoted: m })
-    } else {
-      await conn.sendMessage(m.chat, 'No se pudo encontrar el contacto para enviar el pago.', { quoted: m })
-    }
+    await conn.sendPayment(m.chat, '999999999', texto, m)
+    await conn.sendMessage(m.chat, '💸 Mensaje de venta enviado aquí.', { quoted: m })
   } catch (e) {
     await conn.sendMessage(m.chat, 'Ocurrió un error al enviar el pago.', { quoted: m })
   }
