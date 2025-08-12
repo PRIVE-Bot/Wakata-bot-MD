c647600d22b3ec38737abab55d4029c7e47bfba3
Deylin <deylinbo91@gmail.com>
2025-08-12 15:48:18 -0600

    Actualizar _venta.js

--- plugins/_venta.js
+++ plugins/_venta.js
@@ -17,3 +17,20 @@ handler.tags = ['main']
 handler.command = handler.help = ['buy', 'comprar']
 
 export default handler
+
+
+/*let handler = async (m, { conn }) => {
+  let texto = `Compra *${global.botname}* por $5 para tus grupos y mucho más. ¡Contáctanos para más información!`
+
+  try {
+    await conn.sendPayment(m.chat, '999999999', texto, m)
+    await conn.sendMessage(m.chat, '💸 Mensaje de venta enviado aquí.', { quoted: m })
+  } catch (e) {
+    await conn.sendMessage(m.chat, 'Ocurrió un error al enviar el pago.', { quoted: m })
+  }
+}
+
+handler.tags = ['main']
+handler.command = handler.help = ['buy', 'comprar']
+
+export default handler*/
