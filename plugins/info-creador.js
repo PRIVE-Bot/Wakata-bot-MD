db2ac565ac2fdcb8d16b7b5b6a6a6c65350dc5bb
Davizuni17 <108438114+Davizuni17@users.noreply.github.com>
2025-08-12 19:28:42 -0400

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -3,53 +3,92 @@
 // codigo creado para https://github.com/Deylin-eliac
 // No quites créditos
 
-import PhoneNumber from 'awesome-phonenumber';
+import axios from 'axios'
+const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async (m, { conn }) => {
-  m.react('👑');
+  const proses = '⚘️ Obteniendo información de mi creador...'
+  await conn.sendMessage(m.chat, { text: proses }, { quoted: m })
+
+  async function createImage(url) {
+    const { imageMessage } = await generateWAMessageContent({ image: { url } }, {
+      upload: conn.waUploadToServer
+    })
+    return imageMessage
+  }
 
 
-  const numCreador = '50432955554';
-  const ownerJid = numCreador + '@s.whatsapp.net';
-  const name = await conn.getName(ownerJid) || 'Deylin';
-  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || `Hola, mucho gusto. Soy Deylin.`;
-  const empresa = 'Deylin - Servicios Tecnológicos';
-
-  const vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name};;;
-FN:${name}
-ORG:${empresa};
-TITLE:CEO & Fundador
-TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
-EMAIL:correo@empresa.com
-URL:https://www.tuempresa.com
-NOTE:${about}
-ADR:;;Dirección de tu empresa;;;;
-X-ABADR:ES
-X-ABLabel:Dirección Web
-X-ABLabel:Correo Electrónico
-X-ABLabel:Teléfono de contacto
-X-WA-BIZ-NAME:${name}
-X-WA-BIZ-DESCRIPTION:${about}
-END:VCARD`.trim();
-
-
-  await conn.sendMessage(
-    m.chat,
+  const owners = [
     {
-      contacts: {
-        displayName: name,
-        contacts: [{ vcard }]
-      },
-    },
-    { quoted: m }
-  );
+      name: 'Deylin',
+      desc: 'Creador Principal de Naruto-MD',
+      image: 'https://files.cloudkuimages.guru/images/nDXLaJr1.jpg',
+      buttons: [
+        { name: 'WhatsApp', url: 'https://wa.me/50432955554' },
+        { name: 'Paypal', url: 'https://www.paypal.me/DeylinB' },
+        { name: 'Github', url: 'https://github.com/deylin-eliac' },
+        { name: 'Website', url: 'https://deylin.vercel.app/' }
+      ]
+    }
+  ]
+
+  let cards = []
+
+  for (let owner of owners) {
+    const imageMsg = await createImage(owner.image)
+
+    let formattedButtons = owner.buttons.map(btn => ({
+      name: 'cta_url',
+      buttonParamsJson: JSON.stringify({
+        display_text: btn.name,
+        url: btn.url
+      })
+    }))
+
+    cards.push({
+      body: proto.Message.InteractiveMessage.Body.fromObject({
+        text: `🪴 *${owner.name}*\n${owner.desc}`
+      }),
+      footer: proto.Message.InteractiveMessage.Footer.fromObject({
+        text: '> Conoce más sobre nuestros creadores siguiendo sus redes sociales. Haz clic en cualquier botón para acceder a sus perfiles y descubrir su trabajo. Si te gustaría apoyarlos, también puedes realizar una donación a través de nuestro PayPal.'
+      }),
+      header: proto.Message.InteractiveMessage.Header.fromObject({
+        hasMediaAttachment: true,
+        imageMessage: imageMsg
+      }),
+      nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+        buttons: formattedButtons
+      })
+    })
+  }
+
+  const slideMessage = generateWAMessageFromContent(m.chat, {
+    viewOnceMessage: {
+      message: {
+        messageContextInfo: {
+          deviceListMetadata: {},
+          deviceListMetadataVersion: 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: '⚘️ Creadores de Nino Nakano Ai ⚘️'
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: 'Conoce a los desarrolladores del bot'
+          }),
+          carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
+            cards
+          })
+        })
+      }
+    }
+  }, {})
+
+  await conn.relayMessage(m.chat, slideMessage.message, { messageId: slideMessage.key.id })
 }
 
-handler.help = ['owner'];
-handler.tags = ['main'];
-handler.command = ['owner', 'creator', 'creador', 'dueño'];
+handler.help = ['owner']
+handler.tags = ['info']
+handler.command = /^(donar|owner|cuentasoficiales|creadores|cuentas)$/i
 
-export default handler;
+export default handler
