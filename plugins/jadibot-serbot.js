684eea861cc94c4a41180d00d3ca28129d561614
Deylin <deylinbo91@gmail.com>
2025-08-13 16:12:47 -0600

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -69,7 +69,7 @@ return m.reply(`${emoji2} No se han encontrado espacios para *Sub-Bots* disponib
 /*if (Object.values(global.conns).length === 30) {
 return m.reply(`${emoji2} No se han encontrado espacios para *Sub-Bots* disponibles.`)
 }*/
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+/*let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let id = `${who.split`@`[0]}`  //conn.getName(who)
 let pathJadiBot = path.join(`./${jadi}/`, id)
 if (!fs.existsSync(pathJadiBot)){
@@ -130,7 +130,7 @@ msgRetryCache,
 browser: mcode ? ['Ubuntu', 'Chrome', '110.0.5585.95'] : ['Bot(Sub Bot)', 'Chrome','2.0.0'],
 version: version,
 generateHighQualityLinkPreview: true
-};
+};*/
 
 /*const connectionOptions = {
 printQRInTerminal: false,
