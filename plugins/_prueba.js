2c737c6af6e3161eaf47d23ca1f6ba76b61ef207
Deylin <deylinbo91@gmail.com>
2025-08-18 00:53:02 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,25 +1,22 @@
 
 
 var handler = async (m, { conn, text }) => {
-// thumb3 es tu miniatura
-const res = await fetch('https://files.catbox.moe/oljc0e.png'); // reemplaza con tu imagen
+const res = await fetch('https://files.catbox.moe/oljc0e.png'); 
 const thumb3 = Buffer.from(await res.arrayBuffer());
 
-// El usuario que ejecutó el comando
-let userJid = m.sender; // JID del usuario que ejecutó el comando
+let userJid = m.sender; 
 
-// Fake de mensaje eliminado simulando al usuario
 let fkontak = {
     key: {
         fromMe: false,
         remoteJid: m.chat,
         id: "deletedMessageFake",
-        participant: userJid // Aquí se asigna al usuario que ejecutó el comando
+        participant: userJid 
     },
     message: {
         imageMessage: {
             mimetype: 'image/jpeg',
-            caption: '《✧》Este usuario eliminó un mensaje.',
+            caption: botname ,
             jpegThumbnail: thumb3
         }
     }
