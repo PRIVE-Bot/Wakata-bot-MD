c09edf8935bf2a4b5b151ef169d71052af0c5a9c
Deylin <deylinbo91@gmail.com>
2025-08-17 21:33:46 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -5,9 +5,14 @@ var handler = async (m, { conn, text }) => {
 
   try {
     await m.react('⏳')
-    conn.reply(m.chat, `*${wait}*\n\n> Descargando su aplicación, espere un momento...`, m, fake)
+    let msg = await conn.reply(m.chat, `*${wait}*\n\n> Descargando su aplicación, espere un momento...`, m, fake)
+    
     let searchA = await search(text)
-    if (!searchA.length) return conn.reply(m.chat, '❌ No se encontraron resultados.', m, fake)
+    if (!searchA.length) {
+      await conn.sendMessage(m.chat, { delete: msg.key }) 
+      return conn.reply(m.chat, '❌ No se encontraron resultados.', m, fake)
+    }
+
     let data5 = await download(searchA[0].id)
     let txt = `┏━━━━━━━━━━━━━━━━☒
 ┇➙ *❒ APTOIDE - DESCARGAS* ❑
@@ -17,17 +22,21 @@ var handler = async (m, { conn, text }) => {
 ┋➙ *Update* : ${data5.lastup}
 ┋➙ *Peso* : ${data5.size}
 ┗━━━━━━━━━━━━━━━━⍰`
-await conn.sendMessage(m.chat, { delete: msg.key })
+
+    await conn.sendMessage(m.chat, { delete: msg.key }) 
     await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m)
     await m.react('✅')
+
     if (data5.size.includes('GB') || parseFloat(data5.size.replace(' MB', '')) > 999) {
       return await conn.reply(m.chat, '⚠️ El archivo es demasiado pesado para enviarlo.', m, fake)
     }
+
     await conn.sendMessage(m.chat, {
       document: { url: data5.dllink },
       mimetype: 'application/vnd.android.package-archive',
       fileName: `${data5.name}.apk`
     }, { quoted: m })
+
   } catch (e) {
     console.error(e)
     return conn.reply(m.chat, '❌ Ocurrió un fallo al intentar descargar la APK.', m, fake)
@@ -42,4 +51,3 @@ handler.group = true
 handler.coin = 5
 
 export default handler
-
