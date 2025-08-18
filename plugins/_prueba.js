48e1c7af8457c89952f7b7fb2b260220e5aa3f34
Deylin <deylinbo91@gmail.com>
2025-08-18 16:02:53 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,29 +2,34 @@ import fetch from 'node-fetch'
 import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys'
 
 
-let handler = async (m, {conn}) => {
-const res = await fetch('https://files.catbox.moe/oljc0e.png');
-const img = Buffer.from(await res.arrayBuffer());
+let handler = async (m, { conn }) => {
+  const res = await fetch('https://files.catbox.moe/oljc0e.png');
+  const img = Buffer.from(await res.arrayBuffer());
 
-const fkontak = {
+  const fkontak = {
     key: { 
-        fromMe: false, 
-        remoteJid: "120363368035542631@g.us", 
-        participant: m.sender 
+      fromMe: false, 
+      remoteJid: "120363368035542631@g.us", 
+      participant: m.sender 
     },
     message: {
-        productMessage: {
-            product: {
-                productImage: { jpegThumbnail: img },
-                title: "Membresía Naruto-Bot MD",
-                description: botname ,
-                currencyCode: "USD",
-                priceAmount1000: "5000", 
-                retailerId: "BOT"
-            },
-            businessOwnerJid: "0@s.whatsapp.net"
+      imageMessage: {
+        mimetype: 'image/jpeg',
+        caption: `✨ Membresía Naruto-Bot MD ✨\n${botname}`,
+        jpegThumbnail: img,
+        contextInfo: {
+          externalAdReply: {
+            title: "Membresía Naruto-Bot MD",
+            body: botname,
+            thumbnail: img,
+            sourceUrl: "https://whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F",
+            mediaType: 1,
+            renderLargerThumbnail: true
+          }
         }
+      }
     }
+  };
 };
 
   await conn.sendMessage(
