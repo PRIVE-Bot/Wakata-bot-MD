56200a76816286392ddde3abe85445f8b094c5c5
Deylin <deylinbo91@gmail.com>
2025-08-18 15:23:32 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -6,20 +6,26 @@ let handler = async (m, {conn}) => {
 const res = await fetch('https://files.catbox.moe/oljc0e.png'); 
 const thumb3 = Buffer.from(await res.arrayBuffer());
 
-let fkontak = {
+const fkontak = {
     key: { 
         fromMe: false, 
         remoteJid: "120363368035542631@g.us", 
-        participant: m.sender 
+        participant: m.sender  // 👈 el mismo usuario que ejecuta el comando
     },
     message: {
-        imageMessage: {
-            mimetype: 'image/jpeg',
-            caption: botname,
-            jpegThumbnail: thumb3
+        productMessage: {
+            product: {
+                productImage: { jpegThumbnail: img },
+                title: "𝗠𝗘𝗡𝗨 ＝ 𝗟𝗜𝗦𝗧𝗔 𝗗𝗘 𝗙𝗨𝗡𝗖𝗜𝗢𝗡𝗘𝗦",
+                description: botname,
+                currencyCode: "USD",
+                priceAmount1000: "5000", 
+                retailerId: "BOT"
+            },
+            businessOwnerJid: m.sender
         }
     }
-};
+}
 
   await conn.sendMessage(
     m.chat,
