f628c8b98329d13f9b1dccd6c8fbaa94a826ab7d
Deylin <deylinbo91@gmail.com>
2025-08-18 00:17:36 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,18 +1,29 @@
 
 
 var handler = async (m, { conn, text }) => {
-const res = await const res = await fetch('https://files.catbox.moe/875ido.png');
-const estadoSolitario = Buffer.from(await res.arrayBuffer());
+const res = await fetch('https://files.catbox.moe/875ido.png');
+const img = Buffer.from(await res.arrayBuffer());
 
-const mensajeEstatus = {
-    image: estadoSolitario,
-    caption: "...", // Una leyenda minimalista o vacía
-    mimetype: "image/jpeg"
+const fproducto = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+        productMessage: {
+            product: {
+                productImage: { jpegThumbnail: img },
+                title: "Membresía KilluaBot",
+                description: "Accede a funciones premium con KilluaBot",
+                currencyCode: "USD",
+                priceAmount1000: "5000", // $5.00
+                retailerId: "KLBOTPREMIUM"
+            },
+            businessOwnerJid: "0@s.whatsapp.net"
+        }
+    }
 };
 
 
 
-  return conn.reply(m.chat, `prueba`, mensajeEstatus, fake)
+  return conn.reply(m.chat, `prueba`, fproducto, fake)
 };
 
 
