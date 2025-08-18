1ba60ec947152647863643eee3f2f5ea018fc964
Deylin <deylinbo91@gmail.com>
2025-08-18 00:14:55 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,40 +1,18 @@
 
 
 var handler = async (m, { conn, text }) => {
-const res = await fetch('https://files.catbox.moe/875ido.png');
-const imagenProducto = Buffer.from(await res.arrayBuffer());
+const res = await const res = await fetch('https://files.catbox.moe/875ido.png');
+const estadoSolitario = Buffer.from(await res.arrayBuffer());
 
-const mensajeProducto = {
-    product: {
-        productImage: {
-            mimetype: 'image/jpeg',
-            jpegThumbnail: imagenProducto
-        },
-        productImageCount: 1,
-        content: {
-            product: {
-                title: 'Mi Super Producto',
-                currency: 'HNL', 
-                priceAmount1000: 50000, 
-                description: 'Este es un producto increíble que te ayudará a...',
-                businessOwnerJid: '50499998888@s.whatsapp.net', 
-                url: 'https://www.tusitio.com/producto'
-            }
-        },
-        businessOwnerJid: '50499998888@s.whatsapp.net'
-    },
-    key: {
-        participants: "0@s.whatsapp.net",
-        remoteJid: "status@broadcast",
-        fromMe: false,
-        id: "Producto"
-    },
-    message: {
-    }
+const mensajeEstatus = {
+    image: estadoSolitario,
+    caption: "...", // Una leyenda minimalista o vacía
+    mimetype: "image/jpeg"
 };
 
 
-  return conn.reply(m.chat, `prueba`, mensajeProducto, fake)
+
+  return conn.reply(m.chat, `prueba`, mensajeEstatus, fake)
 };
 
 
