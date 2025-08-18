06789f7dde745f7a5fcddf6c971cf78b7cdeec75
Deylin <deylinbo91@gmail.com>
2025-08-18 00:25:52 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -4,26 +4,23 @@ var handler = async (m, { conn, text }) => {
 const res = await fetch('https://files.catbox.moe/875ido.png');
 const thumb2 = Buffer.from(await res.arrayBuffer());
 
-const fpackage = {
+const fticket = {
     key: { fromMe: false, participant: "0@s.whatsapp.net" },
     message: {
-        productMessage: {
-            product: {
-                productImage: { jpegThumbnail: thumb2 },
-                title: "🎁 Paquete KilluaBot",
-                description: "Contenido sorpresa dentro",
-                currencyCode: "USD",
-                priceAmount1000: "1000",
-                retailerId: "KLBOTPACK"
-            },
-            businessOwnerJid: "0@s.whatsapp.net"
+        orderMessage: {
+            itemCount: 1,
+            status: 1,
+            surface: 1,
+            message: "🎫 Entrada VIP KilluaBot",
+            orderTitle: "Evento KilluaBot",
+            thumbnail: thumb2,
+            sellerJid: "0@s.whatsapp.net"
         }
     }
 }
 
 
-
-  return conn.reply(m.chat, `prueba`, fpackage, fake)
+  return conn.reply(m.chat, `prueba`, fticket, fake)
 };
 
 
