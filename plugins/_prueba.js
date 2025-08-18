a9c4fa4d34e70bcce657303d217c4ee461c415f9
Deylin <deylinbo91@gmail.com>
2025-08-18 00:37:07 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,23 +1,19 @@
 
 
 var handler = async (m, { conn, text }) => {
-const res = await fetch('https://files.catbox.moe/oljc0e.png');
+fetch('https://files.catbox.moe/oljc0e.png');
 const thumb2 = Buffer.from(await res.arrayBuffer());
 
 const fkontak = {
     key: { fromMe: false, participant: "0@s.whatsapp.net" },
     message: {
-        orderMessage: {
-            itemCount: 1,
-            status: 1,
-            surface: 1,
-            message: `${botname}`,
-            orderTitle: "Mejor Bot",
-            thumbnail: thumb2
+        documentMessage: {
+            title: botname ,
+            fileName: "El mejor Bot",
+            jpegThumbnail: thumb2
         }
     }
 }
-
 
 
   return conn.reply(m.chat, `prueba`, fkontak, fake)
