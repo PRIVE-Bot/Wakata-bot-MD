b6fadd99add6abeec9a3270b8eb4c4a87d392698
Deylin <deylinbo91@gmail.com>
2025-08-17 21:27:51 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,4 +1,4 @@
-import { search, download } from 'aptoide-scraper'
+/*import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, { conn, text }) => {
   if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el nombre de la APK para descargar.`, m, fake)
@@ -22,6 +22,64 @@ var handler = async (m, { conn, text }) => {
     if (data5.size.includes('GB') || parseFloat(data5.size.replace(' MB', '')) > 999) {
       return await conn.reply(m.chat, '⚠️ El archivo es demasiado pesado para enviarlo.', m, fake)
     }
+    await conn.sendMessage(m.chat, {
+      document: { url: data5.dllink },
+      mimetype: 'application/vnd.android.package-archive',
+      fileName: `${data5.name}.apk`
+    }, { quoted: m })
+  } catch (e) {
+    console.error(e)
+    return conn.reply(m.chat, '❌ Ocurrió un fallo al intentar descargar la APK.', m, fake)
+  }
+}
+
+handler.help = ['apk <nombre>']
+handler.tags = ['descargas']
+handler.command = ['apk', 'modapk', 'aptoide']
+
+handler.group = true
+handler.coin = 5
+
+export default handler*/
+
+
+import { search, download } from 'aptoide-scraper'
+
+var handler = async (m, { conn, text }) => {
+  if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el nombre de la APK para descargar.`, m, fake)
+
+  try {
+    await m.react('⏳')
+
+    // mensaje inicial
+    let msg = await conn.reply(m.chat, `*${wait}*\n\n> Descargando su aplicación, espere un momento...`, m, fake)
+
+    let searchA = await search(text)
+    if (!searchA.length) return conn.sendMessage(m.chat, { text: '❌ No se encontraron resultados.', edit: msg.key })
+
+    let data5 = await download(searchA[0].id)
+    let txt = `┏━━━━━━━━━━━━━━━━☒
+┇➙ *❒ APTOIDE - DESCARGAS* ❑
+┣━━━━━━━━━━━━━━━━⚄
+┋➙ *Nombre* : ${data5.name}
+┋➙ *Package* : ${data5.package}
+┋➙ *Update* : ${data5.lastup}
+┋➙ *Peso* : ${data5.size}
+┗━━━━━━━━━━━━━━━━⍰`
+
+    // editar mensaje y añadir icono como thumbnail
+    await conn.sendMessage(m.chat, {
+      image: { url: data5.icon },
+      caption: txt,
+      edit: msg.key
+    })
+
+    await m.react('✅')
+
+    if (data5.size.includes('GB') || parseFloat(data5.size.replace(' MB', '')) > 999) {
+      return await conn.reply(m.chat, '⚠️ El archivo es demasiado pesado para enviarlo.', m, fake)
+    }
+
     await conn.sendMessage(m.chat, {
       document: { url: data5.dllink },
       mimetype: 'application/vnd.android.package-archive',
