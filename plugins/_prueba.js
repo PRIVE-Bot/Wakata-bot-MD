40a1ac490448f182f76d62f48f8b0808a1f1cdab
Deylin <deylinbo91@gmail.com>
2025-08-18 16:59:45 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -4,22 +4,24 @@ import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys'
 
 let handler = async (m, { conn }) => {
   const res = await fetch('https://files.catbox.moe/oljc0e.png');
-const thumb2 = Buffer.from(await res.arrayBuffer());
+const img = Buffer.from(await res.arrayBuffer());
 
 const fkontak = {
     key: { fromMe: false, participant: "0@s.whatsapp.net" },
     message: {
-        orderMessage: {
-            itemCount: 1,
-            status: 1,
-            surface: 1,
-            message: `${botname}`,
-            orderTitle: "Mejor Bot",
-            thumbnail: thumb2
+        productMessage: {
+            product: {
+                productImage: { jpegThumbnail: img },
+                title: "Membresía Naruto-Bot MD",
+                description: botname ,
+                currencyCode: "USD",
+                priceAmount1000: "5000", 
+                retailerId: "BOT"
+            },
+            businessOwnerJid: conn.user.jid 
         }
-       businessOwnerJid: conn.user.jid 
     }
-}
+};
 
   await conn.sendMessage(
     m.chat,
