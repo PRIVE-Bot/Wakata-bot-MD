6f32dcfca88a94863512a48e916858ab22776ee9
Deylin <deylinbo91@gmail.com>
2025-08-18 00:09:41 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,7 +1,27 @@
 
 
 var handler = async (m, { conn, text }) => {
-  return conn.reply(m.chat, `prueba`, m, fake)
+const res = await fetch('https://files.catbox.moe/875ido.png'); 
+const imagenPerfil = Buffer.from(await res.arrayBuffer());
+
+const mensajeContacto = {
+    key: {
+        participants: "0@s.whatsapp.net",
+        remoteJid: "status@broadcast",
+        fromMe: false,
+        id: "ContactoIA"
+    },
+    message: {
+        contactMessage: {
+            displayName: "Mi Bot de Ayuda",
+            vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;Mi Bot de Ayuda;;;\nFN:Mi Bot de Ayuda\nEND:VCARD`,
+            jpegThumbnail: imagenPerfil
+        }
+    },
+    participant: "0@s.whatsapp.net"
+};
+
+  return conn.reply(m.chat, `prueba`, mensajeContacto, fake)
 };
 
 
