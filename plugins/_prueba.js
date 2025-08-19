776eb237ac97b8ce31e5b34066c60685ca9aad31
Deylin <deylinbo91@gmail.com>
2025-08-19 00:51:54 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,35 +2,31 @@ import fetch from 'node-fetch'
 import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys'
 
 
-let handler = async (m, { conn }) => {
-  const res = await fetch('https://files.catbox.moe/oljc0e.png');
-const img = Buffer.from(await res.arrayBuffer());
+const res = await fetch('https://files.catbox.moe/f8qrut.png') // tu foto
+const img = Buffer.from(await res.arrayBuffer())
 
-const fkontak = {
-    key: { 
-        fromMe: false, 
-        remoteJid: "120363368035542631@g.us", 
-        participant: m.sender 
+const fakePerfil = {
+  key: { fromMe: false, participant: "0@s.whatsapp.net" },
+  message: {
+    contactMessage: {
+      displayName: "Naruto Uzumaki", // nombre que quieras mostrar
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Naruto Uzumaki\nTEL;type=CELL;type=VOICE;waid=521111111111:+52 1 111 111 1111\nEND:VCARD`
     },
-    message: {
-        productMessage: {
-            product: {
-                productImage: { jpegThumbnail: img },
-                title: "Membresía Naruto-Bot MD",
-                description: botname ,
-                currencyCode: "USD",
-                priceAmount1000: "5000", 
-                retailerId: "BOT"
-            },
-            businessOwnerJid: conn.user.jid 
-        }
+    contextInfo: {
+      externalAdReply: {
+        title: "Naruto-Bot",
+        body: "Audio oficial",
+        thumbnail: img, // aquí va la imagen
+        sourceUrl: "https://whatsapp.com/channel/0029VbB46nl2ER6dZac6Nd1o"
+      }
     }
-};
+  }
+}
 
   await conn.sendMessage(
     m.chat,
     { text: '✨ Estado de ejemplo con estilo de WhatsApp ✨' },
-    { quoted: fkontak }
+    { quoted: fakePerfil }
   )
 }
 handler.command = /^estado$/i
