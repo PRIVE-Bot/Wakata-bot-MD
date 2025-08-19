3c476caab23a12a1623e288039c49a795eb41ccd
Deylin <deylinbo91@gmail.com>
2025-08-19 13:37:39 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,12 +1,8 @@
-import fetch from "node-fetch";
-import { exec } from "child_process";
-import fs from "fs";
-import path from "path";
+
 
 let handler = async (m, { conn }) => {
   
     await conn.reply(m.chat, `Ocurrió un error al enviar el audio.`, m, rcanal1);
-  }
 };
 
 handler.command = /^1$/i;
