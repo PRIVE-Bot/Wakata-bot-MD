0f9d35e83eda54b66a724fe719c44624b3fcae08
Deylin <deylinbo91@gmail.com>
2025-08-19 18:32:41 -0600

    Actualizar tools-read.js

--- plugins/tools-read.js
+++ plugins/tools-read.js
@@ -1,13 +1,26 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `👑 Responde a una imagen ViewOnce.`, m, rcanal);
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `👑 Responde a una imagen ViewOnce.`, m, rcanal);
+const res = await fetch('https://files.catbox.moe/os6bdu.jpg');
+const thumb2 = Buffer.from(await res.arrayBuffer());
+
+const fkontak = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+        documentMessage: {
+            title: '𝗗𝗘𝗦𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢',
+            fileName: `${botname}.mp3`,
+            jpegThumbnail: thumb2
+        }
+    }
+}
+if (!m.quoted) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, rcanal);
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, rcanal);
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m, rcanal);
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', fkontak);
 } else if (/imageMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m, rcanal);
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', fkontak);
 }}
 handler.help = ['ver']
 handler.tags = ['tools']
