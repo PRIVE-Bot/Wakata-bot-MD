44ea23f71d0e702185dab5ed63f8d555d941bcdb
Deylin <deylinbo91@gmail.com>
2025-08-19 22:20:27 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -11,7 +11,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat];
   let botname = global.botname || "Bot";
 
-  const fondoUrl = encodeURIComponent('https://files.catbox.moe/xdz07l.jpg');
+  const fondoUrl = encodeURIComponent('https://files.catbox.moe/ijud3n.jpg');
   const defaultAvatar = encodeURIComponent('https://files.catbox.moe/6al8um.jpg');
 
   let avatarUrl = defaultAvatar;
@@ -26,7 +26,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
       const canvasUrl = `https://gokublack.xyz/canvas/welcome?background=${fondoUrl}&text1=Hola+${taguser.replace('@','')}&text2=Bienvenido&text3=Miembro+${totalMembers}&avatar=${avatarUrl}`;
 
-      const res = await fetch(canvasUrl);
+        const res = await fetch('https://files.catbox.moe/qhxt7c.png');
       const img = Buffer.from(await res.arrayBuffer());
 
       const fkontak = {
@@ -69,7 +69,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
       const canvasUrl = `https://gokublack.xyz/canvas/welcome?background=${fondoUrl}&text1=Adiós+${taguser.replace('@','')}&text2=Hasta+Luego&text3=Miembro+${totalMembers}&avatar=${avatarUrl}`;
 
-      const res = await fetch(canvasUrl);
+      const res = await fetch('https://files.catbox.moe/8alfhv.png');
       const img = Buffer.from(await res.arrayBuffer());
 
       const fkontak1 = {
@@ -108,3 +108,103 @@ export async function before(m, { conn, participants, groupMetadata }) {
     }
   }
 }
+
+
+
+/*import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return true;
+
+  let totalMembers = participants.length;
+  let date = new Date().toLocaleString('es-ES', { timeZone: 'America/Mexico_City' });
+  let who = m.messageStubParameters[0];
+  let taguser = `@${who.split('@')[0]}`;
+  let chat = global.db.data.chats[m.chat];
+  let botname = global.botname || "Bot";
+
+  
+  const res1 = await fetch('https://files.catbox.moe/qhxt7c.png');
+  const img1 = Buffer.from(await res1.arrayBuffer());
+
+  const fkontak = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+      productMessage: {
+        product: {
+          productImage: { jpegThumbnail: img1 },
+          title: `ʙɪᴇɴᴠᴇɴɪᴅᴏ, ᴀʜᴏʀᴀ sᴏᴍᴏs "${totalMembers}"`,
+          description: botname,
+          currencyCode: "USD",
+          priceAmount1000: "5000",
+          retailerId: "BOT"
+        },
+        businessOwnerJid: "0@s.whatsapp.net"
+      }
+    }
+  };
+
+  const res2 = await fetch('https://files.catbox.moe/8alfhv.png');
+  const img2 = Buffer.from(await res2.arrayBuffer());
+
+  const fkontak1 = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+      productMessage: {
+        product: {
+          productImage: { jpegThumbnail: img2 },
+          title: `₳ĐłØ₴ Ʉ₴ɆⱤ ₳ⱧØⱤ₳ ₴Ø₥Ø₴ "${totalMembers}"`,
+          description: botname,
+          currencyCode: "USD",
+          priceAmount1000: "5000",
+          retailerId: "BOT"
+        },
+        businessOwnerJid: "0@s.whatsapp.net"
+      }
+    }
+  };
+
+  let imageUrl =  'https://files.catbox.moe/0183v7.png';
+
+  if (chat.welcome) {
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+      let bienvenida = `
+┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
+┃   ฿łɆ₦VɆ₦łĐØ ✦ ₳
+┣╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚄
+┃ 
+┃  ✎ *Usuario:* ${taguser}  
+┃  ✎ *Grupo:* ${groupMetadata.subject}  
+┃  ✎ *Miembros:* ${totalMembers}  
+┃  ✎ *Fecha:* ${date}  
+┃    
+┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
+
+
+> *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
+> whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F`;
+      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: bienvenida, mentions: [who] }, { quoted: fkontak });
+    }
+
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE ||
+        m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
+      let despedida = `
+┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
+┃             ₳ĐłØ₴ ✦ ₳
+┣╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚄
+┃ 
+┃  ✎ *Usuario:* ${taguser}  
+┃  ✎ *Grupo:* ${groupMetadata.subject}  
+┃  ✎ *Miembros:* ${totalMembers}  
+┃  ✎ *Fecha:* ${date}  
+┃    
+┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
+
+
+> *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
+> whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F`;
+      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: despedida, mentions: [who] }, { quoted: fkontak1 });
+    }
+  }
+}*/
