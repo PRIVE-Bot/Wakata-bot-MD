4933415f82b99e8e412f56d5b09c1921066eda43
Deylin <deylinbo91@gmail.com>
2025-08-19 21:36:10 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,6 +3,47 @@ import { WAMessageStubType } from '@whiskeysockets/baileys'
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
 
+const res = await fetch('https://files.catbox.moe/qhxt7c.png');
+const img = Buffer.from(await res.arrayBuffer());
+
+const fkontak = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+        productMessage: {
+            product: {
+                productImage: { jpegThumbnail: img },
+                title: `ʙɪᴇɴᴠᴇɴɪᴅᴏ, ᴀʜᴏʀᴀ sᴏᴍᴏs ${totalMembers + 1}`,
+                description: botname ,
+                currencyCode: "USD",
+                priceAmount1000: "5000", 
+                retailerId: "BOT"
+            },
+            businessOwnerJid: "0@s.whatsapp.net"
+        }
+    }
+};
+
+const res = await fetch('https://files.catbox.moe/8alfhv.png');
+const img = Buffer.from(await res.arrayBuffer());
+
+const fkontak1 = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+        productMessage: {
+            product: {
+                productImage: { jpegThumbnail: img },
+                title: `₳ĐłØ₴ Ʉ₴ɆⱤ ₳ⱧØⱤ₳ ₴Ø₥Ø₴ ${totalMembers - 1}`,
+                description: botname ,
+                currencyCode: "USD",
+                priceAmount1000: "5000", 
+                retailerId: "BOT"
+            },
+            businessOwnerJid: "0@s.whatsapp.net"
+        }
+    }
+};
+
+
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
@@ -30,7 +71,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
 > *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
 > whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F` 
-      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: bienvenida, mentions: [who] })
+      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: bienvenida, mentions: [who], { quoted: fkontak } })
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE ||
@@ -50,7 +91,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
 > *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
 > whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F` 
-      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: despedida, mentions: [who] })
+      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: despedida, mentions: [who], { quoted: fkontak1 } })
     }
   }
 }
