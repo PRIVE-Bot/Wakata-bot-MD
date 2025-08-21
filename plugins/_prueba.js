8c8fdd0d74a5703483f0f2d11350a2ed094e48dc
Deylin <deylinbo91@gmail.com>
2025-08-21 17:43:19 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,43 +1,26 @@
-import { toPTT, toAudio } from '../lib/converter.js'
-
 let handler = async (m, { conn }) => {
-  try {
-    if (!m.quoted) throw `✳️ Responde a una *nota de voz* o *música*`
 
-    let q = m.quoted
-    let mime = (q.msg || q).mimetype || ''
-    if (!/audio/.test(mime)) throw `❌ Solo funciona con *audios*`
+const res = await fetch('https://files.catbox.moe/oljc0e.png'); 
+const thumb3 = Buffer.from(await res.arrayBuffer());
 
-    let media = await q.download()
-    if (!media) throw `⚠️ No pude descargar el audio`
-
-    let result, filename, mimetype, type
-
-    if (/ogg/.test(mime) && /opus/.test(mime)) {
-      // 📌 Si es nota de voz -> convertir a música
-      result = await toAudio(media, 'ogg')
-      filename = 'audio.opus'
-      mimetype = 'audio/ogg; codecs=opus'
-      type = { audio: result.data, mimetype }
-      await conn.sendMessage(m.chat, type, { quoted: m })
-    } else {
-      // 📌 Si es música -> convertir a nota de voz
-      result = await toPTT(media, mime.split('/')[1])
-      filename = 'ptt.ogg'
-      mimetype = 'audio/ogg; codecs=opus'
-      type = { audio: result.data, mimetype, ptt: true }
-      await conn.sendMessage(m.chat, type, { quoted: m })
+let fkontak = {
+    key: { 
+        fromMe: false, 
+        remoteJid: "120363368035542631@g.us", 
+        participant: m.sender 
+    },
+    message: {
+        imageMessage: {
+            mimetype: 'image/jpeg',
+            caption: botname,
+            jpegThumbnail: thumb3
+        }
     }
+};
 
-    await result.delete?.()
-  } catch (e) {
-    console.error(e)
-    m.reply('⚠️ Error al procesar el audio.')
-  }
-}
+return conn.reply(m.chat, `Por favor, envía un enlace de TikTok para descargar el video.`, m, rcanal);
+    };
 
-handler.help = ['cambiar']
-handler.tags = ['audio']
-handler.command = /^cambiar$/i
 
-export default handler
+
+handler.commad = [''] 
