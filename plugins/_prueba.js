23c844e7f02dbd22c0995de47efa7cac76c8e64d
Deylin <deylinbo91@gmail.com>
2025-08-20 19:25:14 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,35 +1,32 @@
 let handler = async (m, { conn }) => {
   const jid = m.chat;
 
-let fkontak = {
-  try {
-    const productMessage = {
-      product: {
-        productImage: {
-          url: icono
+  
+  const res = await fetch(icono);
+  const img = Buffer.from(await res.arrayBuffer());
+
+  let fkontak = {
+    key: { fromMe: false, participant: "0@s.whatsapp.net" },
+    message: {
+      productMessage: {
+        product: {
+          productImage: { jpegThumbnail: img }, 
+          title: global.textbot || "Mi Producto",
+          description: "software",
+          currencyCode: "USD",
+          priceAmount1000: 5000, 
+          retailerId: "1466",
+          productId: "24502048122733040",
         },
-        title: global.textbot,
-        description: "software",
-        currencyCode: "USD",
-        priceAmount1000: 5000, 
-        retailerId: "1466", 
-        productId: "24502048122733040", 
-        productImageCount: 1,
-      },
-      businessOwnerJid: "50432955554@s.whatsapp.net" 
-        }
+        businessOwnerJid: "50432955554@s.whatsapp.net"
       }
     }
+  };
 
-    
-return conn.reply(m.chat, `Hola`, fkontak);
+  
+  return await conn.sendMessage(m.chat, { text: "Hola" }, { quoted: fkontak });
 };
-
 
 handler.command = ['producto', 'compra', '1'];
 
-
 export default handler;
-
-
-
