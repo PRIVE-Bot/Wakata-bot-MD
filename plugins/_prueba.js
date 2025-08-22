c2ebfa5b34e6684671bc9f539170c8eda738cb41
Deylin <deylinbo91@gmail.com>
2025-08-21 18:32:49 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,23 +1,27 @@
+import fetch from 'node-fetch'
+
 let handler = async (m, { conn }) => {
 
-const res = await fetch('https://files.catbox.moe/cd6i4q.jpg');
-const thumb2 = Buffer.from(await res.arrayBuffer());
+  // Descargar la imagen
+  const res = await fetch('https://files.catbox.moe/cd6i4q.jpg');
+  const thumb2 = Buffer.from(await res.arrayBuffer());
 
-const fkontak = {
+  // Mensaje falso pero normal (imagen + texto)
+  const fkontak = {
     key: { fromMe: false, participant: "0@s.whatsapp.net" },
     message: {
-        documentMessage: {
-            title: 'botname',
-            text: `texto`,
-            jpegThumbnail: thumb2
-        }
+      imageMessage: {
+        mimetype: 'image/jpeg',
+        caption: 'Texto normal aquí 🖼️',
+        jpegThumbnail: thumb2
+      }
     }
-}
+  };
 
-return conn.reply(m.chat, `2`, fkontak, rcanal);
-    };
+  // Responder simulando que respondes a ese mensaje
+  return conn.reply(m.chat, `Hola, este es un reply a un mensaje con imagen 👌`, m, { quoted: fkontak });
 
-
+};
 
 handler.command = ['1'] 
 
