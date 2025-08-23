6ff33fe7c3d2caf293c38de789d82b5e119d3416
Deylin <deylinbo91@gmail.com>
2025-08-22 23:43:33 -0600

    Actualizar ia-edit.js

--- plugins/ia-edit.js
+++ plugins/ia-edit.js
@@ -1,23 +1,32 @@
 import axios from 'axios'
 import fetch from 'node-fetch'
 import FormData from 'form-data'
+import cheerio from 'cheerio'
 
-async function uploadCatboxBuffer(buffer) {
+async function uploadPostImages(buffer) {
   const form = new FormData()
-  form.append('reqtype', 'fileupload')
-  form.append('fileToUpload', buffer, { filename: 'file.jpg', contentType: 'image/jpeg' })
+  form.append('upload_session', Math.random())
+  form.append('file', buffer, { filename: 'file.jpg', contentType: 'image/jpeg' })
 
-  const res = await fetch('https://catbox.moe/user/api.php', {
-    method: 'POST',
-    body: form
+  const res = await axios.post('https://postimages.org/json/rr', form, {
+    headers: form.getHeaders(),
+    maxContentLength: Infinity,
+    maxBodyLength: Infinity
   })
 
-  const url = await res.text()
-  if (!url.startsWith('http')) {
-    console.error("Respuesta de Catbox.moe:", url) 
-    throw new Error('Error al subir imagen a Catbox: ' + url)
+  if (!res.data.url) {
+    throw new Error('Error al subir a PostImages: ' + res.data.error)
   }
-  return url.trim()
+
+  const html = await axios.get(res.data.url)
+  const $ = cheerio.load(html.data)
+  const imageUrl = $('#code_direct').attr('value')
+
+  if (!imageUrl) {
+    throw new Error('No se pudo obtener la URL directa de PostImages.')
+  }
+
+  return imageUrl
 }
 
 let handler = async (m, { text, conn, usedPrefix, command }) => {
@@ -29,9 +38,11 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
     if ((q.mimetype || '').includes('image')) {
       buffer = await q.download()
       if (!text) return m.reply(`Formato:\nResponde/envía imagen con:\n${usedPrefix + command} <prompt>`)
-      url = await uploadCatboxBuffer(buffer)
+     
+      url = await uploadPostImages(buffer)
       prompt = text.trim()
     }
+    
     else if (text && text.includes('|')) {
       const [link, pr] = text.split('|').map(v => v.trim())
       if (!link || !pr) return m.reply(`Formato incorrecto!\nEjemplo:\n${usedPrefix + command} https://files.catbox.moe/abc.jpg | cambia el fondo a negro`)
@@ -40,6 +51,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
     }
     else return m.reply(`Formato:\n${usedPrefix + command} <url> | <prompt>\nO responde/envía imagen con:\n${usedPrefix + command} <prompt>`)
 
+    
     await m.react('✨')
     const apiUrl = `https://api-faa-skuarta.vercel.app/faa/editfoto?url=${encodeURIComponent(url)}&prompt=${encodeURIComponent(prompt)}`
     
