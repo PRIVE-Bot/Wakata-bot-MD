34178b35c7aac6cfa6f5dada51e0d7767c93ece0
Deylin <deylinbo91@gmail.com>
2025-08-25 13:09:32 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -39,23 +39,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       }));
 
 
-/*const res = await fetch('https://files.catbox.moe/d48sk2.jpg');
-const thumb2 = Buffer.from(await res.arrayBuffer());
 
-const fkontak = {
-    key: { 
-        fromMe: false, 
-        remoteJid: "120363368035542631@g.us", 
-        participant: m.sender 
-    },
-    message: {
-        documentMessage: {
-            title: "𝗠𝗘𝗡𝗨 ＝ 𝗟𝗜𝗦𝗧𝗔 𝗗𝗘 𝗙𝗨𝗡𝗖𝗜𝗢𝗡𝗘𝗦",
-            fileName: "Naruto-Bot.pdf",
-            jpegThumbnail: thumb2
-        }
-    }
-}*/
 
 
 const res = await fetch('https://files.catbox.moe/91rqne.jpg');
