fc8b57de770c973bf43d569ba227282b2500f317
Deylin <deylinbo91@gmail.com>
2025-08-26 13:52:48 -0600

    Actualizar tools-read.js

--- plugins/tools-read.js
+++ plugins/tools-read.js
@@ -14,16 +14,17 @@ const fkontak = {
         }
     }
 }
-if (!m.quoted) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, rcanal);
-if (!m?.quoted) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, rcanal);
+if (!m.quoted) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, fake)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, rcanal)
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', fkontak);
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', fkontak)
 } else if (/imageMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', fkontak);
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', fkontak)
 }}
 handler.help = ['ver']
 handler.tags = ['tools']
-handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
+handler.command = ['readviewonce', 'read', 'ver', 'readvo'] 
+handler.register = true 
 
 export default handler
