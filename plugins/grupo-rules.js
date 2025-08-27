7f38f96e65a096be80197bf1c8bd6b12898fe0ab
Deylin <deylinbo91@gmail.com>
2025-08-26 23:39:28 -0600

    Crear grupo-rules.js

---
+++ plugins/grupo-rules.js
@@ -0,0 +1,10 @@
+const handler = async (m, { conn, text, command }) => {
+let metadata = await conn.groupMetadata(m.chat)
+  let reglas = metadata.desc || '*No hay reglas en el grupo.*\n> sigue las reglas comunes de los grupos.'
+return conn.reply(m.chat, `${reglas}`, m, rcanal);
+}
+
+handler.command = handler.help = ['reglas', 'Rules']
+handler.tags = ['group']
+
+export default handler;
