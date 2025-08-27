a5a84f9074d19919a0b447c5cc70b0b7392877dc
Deylin <deylinbo91@gmail.com>
2025-08-26 23:31:55 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn, text, command }) => {
 let metadata = await conn.groupMetadata(m.chat)
-  let reglas = metadata.desc
+  let reglas = metadata.desc || 'No hay reglas en el grupo.'
 return conn.reply(m.chat, `${reglas}`, m, fake);
 }
 
