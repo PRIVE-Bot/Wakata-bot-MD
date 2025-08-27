3c02cd4c3436c6445805614311108ebd54018852
Deylin <deylinbo91@gmail.com>
2025-08-26 23:41:10 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++
@@ -1,8 +0,0 @@
-const handler = async (m, { conn, text, command }) => {
-let metadata = await conn.groupMetadata(m.chat)
-  let reglas = metadata.desc || 'No hay reglas en el grupo.'
-return conn.reply(m.chat, `${reglas}`, m, fake);
-}
-
-handler.command = ['reglas']
-export default handler;
