c867cbbf8062b9b72c7d30f8d7bf7d0a4d9ab59c
Deylin <deylinbo91@gmail.com>
2025-08-28 18:41:37 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -60,8 +60,8 @@ let handler = async (m, { conn }) => {
                 name: "cta_url",
                 buttonParamsJson: JSON.stringify({
                   display_text: "📢 Compartir Spark-Bot",
-                   url: `https://wa.me/${global.ofcbot}`, 
-                  merchant_url: "https://wa.me/50455555555"
+                   url: `https://wa.me/?text=🔥+Prueba+SPARK-BOT+ahora!+Entra+al+canal:+https://chat.whatsapp.com/HuMh41LJftl4DH7G5MWcHP`, 
+                  merchant_url: "https://wa.me"
                 })
               }
             ]
