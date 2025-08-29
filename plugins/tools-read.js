b31903b0446d5274699ad1e22365d2f9d19eb3ed
Deylin <deylinbo91@gmail.com>
2025-08-29 00:06:53 -0600

    Actualizar tools-read.js

--- plugins/tools-read.js
+++ plugins/tools-read.js
@@ -1,30 +1,16 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-const res = await fetch('https://files.catbox.moe/os6bdu.jpg');
-const thumb2 = Buffer.from(await res.arrayBuffer());
-
-const fkontak = {
-    key: { fromMe: false, participant: "0@s.whatsapp.net" },
-    message: {
-        documentMessage: {
-            title: '𝗗𝗘𝗦𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢',
-            fileName: `𝗗𝗘𝗦𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢`,
-            jpegThumbnail: thumb2
-        }
-    }
-}
-if (!m.quoted) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, fake)
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `${emoji} Responde a una imagen ViewOnce.`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, `Responde a una imagen ViewOnce.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `Responde a una imagen ViewOnce.`, m, fkontak)
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', fkontak)
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
 } else if (/imageMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', fkontak)
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
 }}
-handler.help = ['ver']
-handler.tags = ['tools']
-handler.command = ['readviewonce', 'read', 'ver', 'readvo'] 
-handler.register = true 
+
+handler.command = ['readviewonce', 'read', 'readvo', 'rvo', 'ver'] 
+handler.group = true;
 
 export default handler
