d732f85aae1bd9b50e0c2c2a651b6a2e15f9cdb9
Deylin <deylinbo91@gmail.com>
2025-08-29 00:10:33 -0600

    Actualizar tools-read.js

--- plugins/tools-read.js
+++ plugins/tools-read.js
@@ -1,15 +1,28 @@
-let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
-
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `Responde a una imagen ViewOnce.`, m)
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `Responde a una imagen ViewOnce.`, m, fkontak)
-let buffer = await m.quoted.download(false);
-if (/videoMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
-} else if (/imageMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
-}}
+  if (!m.quoted) return conn.reply(m.chat, `Responde a una imagen o video ViewOnce.`, m)
 
-handler.command = ['readviewonce', 'read', 'readvo', 'rvo', 'ver'] 
+  
+  let q = m.quoted
+  let msg = q.msg || q
 
+  if (!(msg.viewOnceMessage || msg.viewOnceMessageV2)) {
+    return conn.reply(m.chat, `Responde a una imagen o video ViewOnce.`, m)
+  }
+
+  let media = msg.viewOnceMessage?.message || msg.viewOnceMessageV2?.message
+  let type = Object.keys(media)[0]
+  let buffer = await downloadContentFromMessage(media[type], type.replace('Message', ''))
+
+  let chunks = []
+  for await (const chunk of buffer) chunks.push(chunk)
+  let result = Buffer.concat(chunks)
+
+  if (/videoMessage/.test(type)) {
+    return conn.sendFile(m.chat, result, 'media.mp4', media[type]?.caption || '', m)
+  } else if (/imageMessage/.test(type)) {
+    return conn.sendFile(m.chat, result, 'media.jpg', media[type]?.caption || '', m)
+  }
+}
+
+handler.command = ['readviewonce','read','readvo','rvo','ver']
 export default handler
