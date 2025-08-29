91e2a935caf96c63ffa2a3911b82d4ebfcf7a897
Deylin <deylinbo91@gmail.com>
2025-08-29 12:32:26 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,19 +2,15 @@ import { createMessageWithReactions } from '../lib/reaction.js';
 
 let handler = async (m, { conn }) => {
     const actions = {
-        
         'ℹ️': { type: 'exec_command', data: { command: '.info' } },
-        
         '📝': { type: 'exec_command', data: { command: '.help' } },
     };
-
     const infoMessage = `
 Reacciona para ver más información.
 
 ℹ️ = Ver info del bot
 📝 = Ver lista de comandos
 `;
-
     const msg = await conn.reply(m.chat, infoMessage, m);
     await createMessageWithReactions(conn, msg, actions);
 };
