8d811ee261bc74617d487041ede6d6047f714453
Deylin <deylinbo91@gmail.com>
2025-08-30 21:36:58 -0600

    Actualizar tools-read.js

--- plugins/tools-read.js
+++ plugins/tools-read.js
@@ -1,8 +1,6 @@
 import { downloadContentFromMessage } from '@whiskeysockets/baileys'
 
 let handler = async (m, { conn }) => {
-const res = await fetch('https://i.postimg.cc/rsgTTn6K/1756602958244.jpg');
-const thumb2 = Buffer.from(await res.arrayBuffer());
 
     const fkontak = {
       key: { fromMe: false, participant: "0@s.whatsapp.net" },
@@ -13,33 +11,32 @@ const thumb2 = Buffer.from(await res.arrayBuffer());
           surface: 1,
           message: `𝗗𝗘𝗦𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢`,
           orderTitle: "Mejor Bot",
-          jpegThumbnail: thumb2
         }
       }
     };
 
-  if (!m.quoted) return conn.reply(m.chat, 'Responde a una imagen o video ViewOnce.', m, fake)
+    if (!m.quoted) return conn.reply(m.chat, 'Responde a una imagen o video ViewOnce.', m, fake);
 
-  let quoted = m.quoted.message
-  if (!quoted) return conn.reply(m.chat, 'No se pudo obtener el mensaje citado.', m, fake)
+    let quoted = m.quoted.message;
+    if (!quoted) return conn.reply(m.chat, 'No se pudo obtener el mensaje citado.', m, fake);
 
-  let type = Object.keys(quoted)[0] 
-  if (!['imageMessage','videoMessage'].includes(type)) 
-    return conn.reply(m.chat, 'Responde a una imagen o video ViewOnce.', m, fake)
+    let type = Object.keys(quoted)[0];
+    if (!['imageMessage', 'videoMessage'].includes(type))
+      return conn.reply(m.chat, 'Responde a una imagen o video ViewOnce.', m, fake);
 
-  await m.react('⚡️');
-  let media = quoted[type]
+    await m.react('⚡️');
+    let media = quoted[type];
 
-  let stream = await downloadContentFromMessage(media, type.replace('Message',''))
-  let buffer = Buffer.concat([])
-  for await (const chunk of stream) buffer = Buffer.concat([buffer, chunk])
+    let stream = await downloadContentFromMessage(media, type.replace('Message', ''));
+    let buffer = Buffer.concat([]);
+    for await (const chunk of stream) buffer = Buffer.concat([buffer, chunk]);
 
-  if (type === 'videoMessage') {
-    await conn.sendFile(m.chat, buffer, 'media.mp4', media?.caption || '', fkontak)
-  } else if (type === 'imageMessage') {
-    await conn.sendFile(m.chat, buffer, 'media.jpg', media?.caption || '', fkontak)
-  }
+    if (type === 'videoMessage') {
+      await conn.sendFile(m.chat, buffer, 'media.mp4', media?.caption || '', fkontak);
+    } else if (type === 'imageMessage') {
+      await conn.sendFile(m.chat, buffer, 'media.jpg', media?.caption || '', fkontak);
+    }
 }
 
-handler.command = ['readviewonce','read','readvo','rvo','ver']
-export default handler
+handler.command = ['readviewonce', 'read', 'readvo', 'rvo', 'ver'];
+export default handler;
