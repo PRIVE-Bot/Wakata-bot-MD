fc8eff3417f02182df558689a21358c86fa51192
Deylin <deylinbo91@gmail.com>
2025-08-30 22:21:40 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,5 +1,21 @@
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-    return conn.reply(m.chat, ` prueba `, m)
+const res = await fetch('https://postimg.cc/vg3KfN7T/b98b26f9');
+const thumb2 = Buffer.from(await res.arrayBuffer());
+
+    const fkontak = {
+      key: { fromMe: false, participant: "0@s.whatsapp.net" },
+      message: {
+        orderMessage: {
+          itemCount: 1,
+          status: 1,
+          surface: 1,
+          message: `prueba`,
+          orderTitle: "Mejor Bot",
+          jpegThumbnail: thumb2
+        }
+      }
+    };
+    return conn.reply(m.chat, ` prueba `, fkontak)
 };
 
 handler.command = ['1']
