56528f6c397e16fa8576cf798aaed1765ab675c6
Deylin <deylinbo91@gmail.com>
2025-08-30 22:59:40 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,18 +1,10 @@
-import fetch from "node-fetch";
-
 const handler = async (m, { conn }) => {
-  // Descargar la imagen que quieres mostrar
-  const res = await fetch("https://i.postimg.cc/vg3KfN7T/b98b26f9.jpg");
-  const thumbBuffer = Buffer.from(await res.arrayBuffer());
-
-  // Mensaje tipo ubicación con miniatura personalizada
   const message = {
     location: {
-      degreesLatitude: 0,   // no importa, puede ser 0
-      degreesLongitude: 0,  // no importa, puede ser 0
-      name: "Mi ubicación personalizada", // título de la ubicación
-      address: "El amor, en su esencia más pura, es un refugio y un faro; pero en la oscuridad de la depresión, a veces se siente como una verdad inalcanzable, o peor aún, como un espejo que solo refleja la distancia entre el mundo que anhelas y la batalla silenciosa que liberas dentro de ti.
-" // descripción del mensaje
+      degreesLatitude: 14.0818,   // latitud de ejemplo
+      degreesLongitude: -87.2068, // longitud de ejemplo
+      name: "Mi ubicación personalizada",
+      address: `El amor, en su esencia más pura, es un refugio y un faro; pero en la oscuridad de la depresión, a veces se siente como una verdad inalcanzable, o peor aún, como un espejo que solo refleja la distancia entre el mundo que anhelas y la batalla silenciosa que liberas dentro de ti.`
     }
   };
 
