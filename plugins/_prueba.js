867ee66a49141c715ae2adc83a87624f63bcdc97
Deylin <deylinbo91@gmail.com>
2025-09-01 22:58:17 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,38 +1,13 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
+let handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, {
+    location: {
+      degreesLatitude: 15.2000,
+      degreesLongitude: -86.3000,
+      name: "📍 ¡Descubre el bot más rápido!",
+      address: "Disponible ahora por $5"
+    }
+  }, { quoted: m })
+}
 
-const handler = async (m, { conn }) => {
-    const user = m.sender;
-
-    const content = {
-        viewOnceMessage: {
-            message: {
-                interactiveMessage: {
-                    body: { text: `No tengas miedo y presiona el botón y selecciona tus grupos.` },
-                    footer: { text: "sin miedo al éxito" },
-                    header: { title: "Ándale", hasMediaAttachment: false },
-                    nativeFlowMessage: {
-                        buttons: [
-                            {
-                                name: "cta_url",
-                                buttonParamsJson: JSON.stringify({
-                                    display_text: "Tocame y mira lo que pasa.",
-                                    url: `https://wa.me/?text=🔥+Prueba+SPARK-BOT+ahora!+Entra+al+grupo:+https://chat.whatsapp.com/HuMh41LJftl4DH7G5MWcHP`,
-                                    merchant_url: "https://wa.me"
-                                })
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-    };
-
-    const msg = generateWAMessageFromContent(m.chat, content, { quoted: m });
-    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
-
-    //m.reply("✅ Mensaje enviado.");
-};
-
-handler.command = ['1'];
-
-export default handler;
+handler.command = ['promo1']
+export default handler
