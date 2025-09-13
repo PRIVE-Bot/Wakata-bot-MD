a574cae316d0204ee0e8a9ba4d454c80eecf5001
Deylin <deylinbo91@gmail.com>
2025-09-13 00:42:29 -0600

    Actualizar _antisubbot.js

--- plugins/_antisubbot.js
+++ plugins/_antisubbot.js
@@ -1,6 +1,6 @@
 //import { areJidsSameUser } from '@whiskeysockets/baileys';
 
-export async function before(m, { participants, conn }) {
+/*export async function before(m, { participants, conn }) {
   if (m.isGroup) {
     let chat = global.db.data.chats[m.chat];
 
@@ -27,6 +27,27 @@ export async function before(m, { participants, conn }) {
         }, 5000) 
       }
     }
+  }
+}*/
+
+import { areJidsSameUser } from '@whiskeysockets/baileys';
+
+export async function before(m, { participants, conn }) {
+  if (!m.isGroup) return;
+
+  let chat = global.db.data.chats[m.chat];
+  if (!chat.antiBot2) return;
+
+  let botJid = global.conn.user.jid;
+
+  if (botJid === conn.user.jid) return;
+
+  let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id));
+
+  if (isBotPresent) {
+    // await conn.reply(m.chat, `Ya hay un bot principal activo, no responderé comandos.`, m);
+
+    return true; 
   }
 }
 
