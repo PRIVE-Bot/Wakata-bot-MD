1fdeb61c71385829fc6d5ae3f8f71aeb0acf7a28
Deylin <deylinbo91@gmail.com>
2025-09-12 21:30:21 -0600

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,9 +1,8 @@
-/*export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
-  if (m.text.includes('code') || m.text.includes('PAPEL') || m.text.includes('qr') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
-
+  if (m.text.includes('menu') || m.text.includes('p') || m.text.includes('buy') || m.text.includes('qr') || m.text.includes('code')) return !0;
 const res = await fetch('https://i.postimg.cc/6pjTJPXJ/1755843251131.png');
 const img = Buffer.from(await res.arrayBuffer());
 
@@ -24,27 +23,16 @@ const fkontak = {
     }
 };
 
-
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
-if (m.chat === '120363402601912509@newsletter') return !0
+if (m.chat) return !0
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(
-`
-█████████████████████████
-█     ${botname}     
-█████████████████████████
+    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, 
 
-👋 Hola @${m.sender.split`@`[0]},
 
 ⚠️ Los comandos no funcionan en *privados*.  
-Serás *bloqueado* inmediatamente.
-
-🔥 Usa mis funciones en el  grupo principal:  
-
-█████████████████████████
-`, fkontak, { mentions: [m.sender] });
+Serás *bloqueado* inmediatamente.`, fkontak, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
-}*/
+}
