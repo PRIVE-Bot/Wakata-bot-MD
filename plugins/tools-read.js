14fed54dfe01180170a2aa40eab5cdd92896796d
Deylin <deylinbo91@gmail.com>
2025-09-14 00:17:10 -0600

    Actualizar tools-read.js

--- plugins/tools-read.js
+++ plugins/tools-read.js
@@ -1,71 +1,17 @@
-import { downloadContentFromMessage } from '@whiskeysockets/baileys';
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-    const fkontak = {
-        key: { fromMe: false, participant: "0@s.whatsapp.net" },
-        message: {
-            orderMessage: {
-                itemCount: 1,
-                status: 1,
-                surface: 1,
-                message: "𝗗𝗘𝗦𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢",
-                orderTitle: "Mejor Bot",
-            }
-        }
-    };
+if (!m.quoted) return conn.reply(m.chat, `${emojis} Responde a una imagen ViewOnce.`, m, rcanal)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `${emojis} Responde a una imagen ViewOnce.`, m, rcanal)
+let buffer = await m.quoted.download(false);
+if (/videoMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+} else if (/imageMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
+}}
+handler.help = ['ver']
+handler.tags = ['tools']
+handler.command = ['readviewonce', 'read', 'readvo', 'ver', 'pesquisar'] 
+handler.register = true 
 
-    const quoted = m.quoted;
-
-    if (!quoted) {
-        return conn.reply(m.chat, `*❗ Responde a un mensaje de "ver una vez".*`, m, { contextInfo: { mentionedJid: [m.sender] } });
-    }
-    
-    const msg = quoted.message;
-    const viewOnceKey = Object.keys(msg).find(key => key.includes('viewOnceMessage'));
-    
-    if (!viewOnceKey) {
-        return conn.reply(m.chat, `*❗ Responde a un mensaje de "ver una vez".*`, m, { contextInfo: { mentionedJid: [m.sender] } });
-    }
-
-    try {
-        const viewOnceMessage = msg[viewOnceKey].message;
-        const type = Object.keys(viewOnceMessage)[0];
-        const buffer = await downloadContentFromMessage(viewOnceMessage[type], type.slice(0, -7));
-
-        let fileExtension;
-        let mime;
-
-        if (type === 'imageMessage') {
-            fileExtension = 'jpg';
-            mime = 'image/jpg';
-        } else if (type === 'videoMessage') {
-            fileExtension = 'mp4';
-            mime = 'video/mp4';
-        } else if (type === 'audioMessage') {
-            fileExtension = 'mp3';
-            mime = 'audio/mp3';
-        } else {
-            return conn.reply(m.chat, `*❗ Este tipo de mensaje no es compatible para desbloquear.*`, m);
-        }
-
-        const media = Buffer.from([]);
-        for await (const chunk of buffer) {
-            media.push(chunk);
-        }
-
-        await conn.sendFile(m.chat, media, `media.${fileExtension}`, quoted.caption || '', fkontak, false, {
-            mimetype: mime,
-            ptt: type === 'audioMessage' ? viewOnceMessage.audioMessage.ptt : false
-        });
-
-    } catch (e) {
-        await conn.reply(m.chat, `*❌ Ocurrió un error al intentar desbloquear el mensaje:*\n\n\`\`\`${e.message}\`\`\``, m);
-    }
-};
-
-handler.help = ['ver'];
-handler.tags = ['tools'];
-handler.command = ['readviewonce', 'read', 'readvo', 'ver'];
-handler.register = true;
-
-export default handler;
+export default handler
