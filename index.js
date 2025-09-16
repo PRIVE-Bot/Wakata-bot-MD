40c0a15e2fb79912f55c45bc1af80c684ae21406
Deylin <deylinbo91@gmail.com>
2025-09-16 01:31:20 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import yargs from 'yargs'
 import { spawn, execSync } from 'child_process'
 import lodash from 'lodash'
 import { JadiBot } from './plugins/jadibot-serbot.js'
-import { setupErrorHandler } from "./plugins/tools-error.js"
+import { setupErrorHandler } from './plugins/tools-arror.js'
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import { tmpdir } from 'os'
