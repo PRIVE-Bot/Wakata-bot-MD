1865330d6bbe49fe87ffcb3f76230ce7a2f49e08
Deylin <deylinbo91@gmail.com>
2025-09-17 01:30:25 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -37,13 +37,13 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   if (!tipo) return
 
+let avatar = await conn.profilePictureUrl(who).catch(() => tipo2)
+let urlapi = `https://canvas-8zhi.onrender.com/api/welcome?title=${encodeURIComponent(tipo)}&desc=${encodeURIComponent(tipo1)}&profile=${encodeURIComponent(avatar)}&background=${encodeURIComponent(tipo2)}`
+
   let fkontak
   try {
     const res2 = await fetch('https://i.postimg.cc/c4t9wwCw/1756162596829.jpg')
     const img3 = Buffer.from(await res2.arrayBuffer())
-
-let avatar = await conn.profilePictureUrl(who).catch(() => tipo2)
-let urlapi = 'https://canvas-8zhi.onrender.com/api/welcome?title=${encodeURIComponent(tipo)}&desc=${encodeURIComponent(tipo1)}&profile=${encodeURIComponent(avatar)}&background=${encodeURIComponent(tipo2)}'
 
     fkontak = {
       key: { fromMe: false, participant: "0@s.whatsapp.net" },
