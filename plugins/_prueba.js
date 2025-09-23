a8f7dd636cf2bff87d22a0dac95447446d896edc
Deylin <deylinbo91@gmail.com>
2025-09-23 00:06:55 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 import baileys from '@whiskeysockets/baileys';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `❗️Uso: ${usedPrefix + command} <texto>\nEjemplo: ${usedPrefix + command} anime matando`, m);
+  if (!text) return conn.reply(m.chat, `${emoji} Falta el texto para iniciar la búsqueda de GIF.`, m, fakel);
 
   try {
-    // Buscamos los GIFs en Tenor
     const { data } = await axios.get(
       `https://api.tenor.com/v1/search?q=${encodeURIComponent(text)}&key=LIVDSRZULELA&limit=5`
     );
@@ -13,13 +12,11 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!data?.results || data.results.length === 0)
       return conn.reply(m.chat, `❌ No encontré GIFs para *${text}*`, m);
 
-    // Enviamos los GIFs uno por uno
     for (let gif of data.results) {
       const mediaObj = gif.media[0];
       const url = mediaObj?.mp4?.url || mediaObj?.gif?.url || mediaObj?.tinygif?.url;
 
-      if (!url) continue; // si no hay URL, saltamos
-
+      if (!url) continue; 
       await conn.sendMessage(m.chat, {
         video: { url },
         mimetype: 'video/mp4',
