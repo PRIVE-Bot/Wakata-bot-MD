04d5edc23efd235a767e0623e60957ce17705a81
Deylin <deylinbo91@gmail.com>
2025-09-24 23:08:47 -0600

    Actualizar owner-get.js

--- plugins/owner-get.js
+++ plugins/owner-get.js
@@ -1,29 +1,36 @@
 import fetch from 'node-fetch'
-import { format } from 'util'
 
 let handler = async (m, { conn, text }) => {
-if (m.fromMe) return
-if (!/^https?:\/\//.test(text)) return m.reply(`${emoji} Por favor, ingresa la *url* de la pagina.`)
-let url = text
-await m.react('🕒')
-let res = await fetch(url)
-if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
-throw `Content-Length: ${res.headers.get('content-length')}`
-}
-if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
-let txt = await res.buffer()
-try {
-txt = format(JSON.parse(txt + ''))
-} catch (e) {
-txt = txt + ''
-} finally {
-m.reply(txt.slice(0, 65536) + '')
-await m.react('✔️')
-}}
+  if (m.fromMe) return
+  if (!/^https?:\/\//.test(text)) return m.reply('Por favor, ingresa la *URL* de la página o archivo.')
 
-handler.help = ['get']
+  let url = text
+  await m.react('🕒')
+
+  try {
+    let res = await fetch(url)
+    let contentType = res.headers.get('content-type') || ''
+    let data = await res.buffer()
+
+    if (/text|json/.test(contentType)) {
+      let txt = data.toString('utf-8')
+      try {
+        txt = JSON.stringify(JSON.parse(txt), null, 2)
+      } catch {}
+      await m.reply(txt)
+    } else {
+      await conn.sendFile(m.chat, data, 'archivo', '', m)
+    }
+
+    await m.react('✔️')
+  } catch (e) {
+    await m.reply('Ocurrió un error al obtener el archivo: ' + e)
+  }
+}
+
+handler.help = ['get', 'fetch']
 handler.tags = ['tools']
 handler.command = ['fetch', 'get']
-handler.rowner = true;
+handler.rowner = true
 
 export default handler
