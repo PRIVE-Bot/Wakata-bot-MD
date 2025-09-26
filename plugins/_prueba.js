5a44448a42672b60fb59837004552c6b3615691e
Deylin <deylinbo91@gmail.com>
2025-09-25 20:42:25 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,16 +1,14 @@
-import MessageType from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 import fs from "fs"
+
 const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-fetch(url, options)
-.then(response => response.json())
-.then(json => {
-resolve(json)
+  fetch(url, options)
+    .then(response => response.json())
+    .then(json => resolve(json))
+    .catch(err => reject(err))
 })
-.catch((err) => {
-reject(err)
-})})
+
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   const res1 = await fetch('https://files.catbox.moe/p87uei.jpg')
   const thumb5 = Buffer.from(await res1.arrayBuffer())
@@ -26,13 +24,21 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
     }
   }
 
-if (!args[0]) return conn.reply(m.chat, `${emoji} Mal usó del comando, Ejemplo: *${usedPrefix + command}* 😎+🤑`, m)
-let [emoji, emoji2] = text.split`+`
-let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
-for (let res of anu.results) {
-let stiker = await sticker(false, res.url, global.botname, global.nombre)
-conn.sendFile(m.chat, stiker, null, { asSticker: true }, fkontak)
-}}
+  if (!args[0]) return conn.reply(m.chat, `${emojis} Uso incorrecto.\nEjemplo: *${usedPrefix + command} 😎+🤑*`, m, fake)
+
+  let [emoji, emoji2] = text.split`+`
+  if (!emoji2) return conn.reply(m.chat, `⚠️ Debes escribir dos emojis separados con +`, m)
+
+  let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
+
+  for (let res of anu.results) {
+    let img = res.media_formats?.png_transparent?.url || res.media_formats?.webp_transparent?.url
+    if (!img) continue
+    let stiker = await sticker(false, img, global.botname || "Bot", global.nombre || "Sticker")
+    await conn.sendMessage(m.chat, { sticker: stiker }, { quoted: fkontak })
+  }
+}
+
 handler.help = ['emojimix *<emoji+emoji>*']
 handler.tags = ['sticker']
 handler.command = ['emojimix'] 
