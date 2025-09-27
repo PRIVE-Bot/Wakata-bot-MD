45e5fd19c891c16e6add3567531c6bf9a3e3fafe
Deylin <deylinbo91@gmail.com>
2025-09-26 21:28:05 -0600

    Crear demote.js

---
+++ plugins/grupo/demote.js
@@ -0,0 +1,56 @@
+var handler = async (m, { conn,usedPrefix, command, text }) => {
+
+if (isNaN(text) && !text.match(/@/g)){
+
+} else if (isNaN(text)) {
+var number = text.split`@`[1]
+} else if (!isNaN(text)) {
+var number = text
+}
+
+    const res = await fetch('https://files.catbox.moe/9xene9.jpg');
+    const thumb2 = Buffer.from(await res.arrayBuffer());
+
+    const fkontak = {
+        key: {
+            participants: "0@s.whatsapp.net",
+            remoteJid: "status@broadcast",
+            fromMe: false,
+            id: "Halo"
+        },
+        message: {
+            locationMessage: {
+                name: '𝗨𝗡 𝗔𝗗𝗠𝗜𝗡 𝗠𝗘𝗡𝗢𝗦',
+                jpegThumbnail: thumb2
+            }
+        },
+        participant: "0@s.whatsapp.net"
+    };
+
+if (!text && !m.quoted) return conn.reply(m.chat, `${emoji} Debes mencionar a un usuario para poder degradarlo de administrador.`, m, fake)
+if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `${emoji} Debes mencionar a un usuario para poder degradarlo de administrador.`, m, fake)
+
+try {
+if (text) {
+var user = number + '@s.whatsapp.net'
+} else if (m.quoted.sender) {
+var user = m.quoted.sender
+} else if (m.mentionedJid) {
+var user = number + '@s.whatsapp.net'
+} 
+} catch (e) {
+} finally {
+conn.groupParticipantsUpdate(m.chat, [user], 'demote')
+conn.reply(m.chat, `${emoji}\n *Fue descartado como admin.*`, fkontak, fake)
+}
+
+}
+handler.help = ['demote']
+handler.tags = ['grupo']
+handler.command = ['demote','quitarpija', 'degradar']
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+
+export default handler
