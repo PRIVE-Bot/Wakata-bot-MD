5619c4638b84aa3241238913dd8f4670070f8dba
Deylin <deylinbo91@gmail.com>
2025-09-27 23:08:55 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -15,7 +15,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   if (!chat.welcome) return
 
-  let userName = await conn.getName(who).catch(() => "Usuario desconocido")
+  let userName = m.pushName || 'Anónimo'
   let tipo = ''
   let tipo1 = ''
   let tipo2 = ''
@@ -52,7 +52,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
           product: {
             productImage: { jpegThumbnail: img3 },
             title: `${tipo} ${tipo1}`,
-            description: `${botname} da la bienvenida a ${taguser}`,
+            description: `${botname} da la bienvenida a ${userName}`,
             currencyCode: "USD",
             priceAmount1000: 5000,
             retailerId: "BOT"
@@ -79,7 +79,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
       currencyCode: "USD",
       priceAmount1000: 5000,
       retailerId: "1677",
-      productId: "2452603047038430",
+      productId: "24526030470358430",
       productImageCount: 1,
     },
     businessOwnerJid: "5043295554@s.whatsapp.net"
@@ -90,232 +90,3 @@ export async function before(m, { conn, participants, groupMetadata }) {
     contextInfo: { mentionedJid: [who] }
   })
 }
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-/*import { WAMessageStubType } from '@whiskeysockets/baileys';
-import fetch from 'node-fetch';
-
-export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return true;
-
-  let totalMembers = participants.length;
-  let date = new Date().toLocaleString('es-ES', { timeZone: 'America/Mexico_City' });
-  let who = m.messageStubParameters[0];
-  let taguser = `@${who.split('@')[0]}`;
-  let chat = global.db.data.chats[m.chat];
-  let botname = global.botname || "Bot";
-
-  const fondoUrl = encodeURIComponent('https://files.catbox.moe/ijud3n.jpg');
-  const defaultAvatar = encodeURIComponent('https://files.catbox.moe/6al8um.jpg');
-
-  let avatarUrl = defaultAvatar;
-  try {
-    const userProfilePic = await conn.profilePictureUrl(who, 'image');
-    avatarUrl = encodeURIComponent(userProfilePic);
-  } catch (e) {
-    avatarUrl = defaultAvatar; 
-  }
-
-  if (chat.welcome) {
-    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      const canvasUrl = `https://gokublack.xyz/canvas/welcome?background=${fondoUrl}&text1=Hola+user&text2=Bienvenido&text3=Miembro+${totalMembers}&avatar=${avatarUrl}`;
-
-        const res2 = await fetch('https://files.catbox.moe/qhxt7c.png');
-      const img2 = Buffer.from(await res2.arrayBuffer());
-const res = await fetch(canvasUrl);
-      const img = Buffer.from(await res.arrayBuffer());
-
-      const fkontak = {
-        key: { fromMe: false, participant: "0@s.whatsapp.net" },
-        message: {
-          productMessage: {
-            product: {
-              productImage: { jpegThumbnail: img3 },
-              title: `ʙɪᴇɴᴠᴇɴɪᴅᴏ, ᴀʜᴏʀᴀ sᴏᴍᴏs "${totalMembers}"`,
-              description: botname,
-              currencyCode: "USD",
-              priceAmount1000: "5000",
-              retailerId: "BOT"
-            },
-            businessOwnerJid: "0@s.whatsapp.net"
-          }
-        }
-      };
-
-      let bienvenida = `
-┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-┃   ฿łɆ₦VɆ₦łĐØ ✦ ₳
-┣╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚄
-┃ 
-┃  ✎ *Usuario:* ${taguser}  
-┃  ✎ *Grupo:* ${groupMetadata.subject}  
-┃  ✎ *Miembros:* ${totalMembers}  
-┃  ✎ *Fecha:* ${date}  
-┃    
-┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-
-> *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
-> whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F`;
-
-      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] }, { quoted: fkontak });
-    }
-
-    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE ||
-        m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
-
-      const canvasUrl = `https://gokublack.xyz/canvas/welcome?background=${fondoUrl}&text1=Adiós+del+grupo&text2=Hasta+Luego&text3=Miembro+${totalMembers}&avatar=${avatarUrl}`;
-
-      const res1 = await fetch('https://files.catbox.moe/8alfhv.png');
-      const img3 = Buffer.from(await res1.arrayBuffer());
-const res = await fetch(canvasUrl);
-      const img = Buffer.from(await res.arrayBuffer());
-
-
-      const fkontak1 = {
-        key: { fromMe: false, participant: "0@s.whatsapp.net" },
-        message: {
-          productMessage: {
-            product: {
-              productImage: { jpegThumbnail: img3 },
-              title: `₳ĐłØ₴ Ʉ₴ɆⱤ ₳ⱧØⱤ₳ ₴Ø₥Ø₴ "${totalMembers}"`,
-              description: botname,
-              currencyCode: "USD",
-              priceAmount1000: "5000",
-              retailerId: "BOT"
-            },
-            businessOwnerJid: "0@s.whatsapp.net"
-          }
-        }
-      };
-
-      let despedida = `
-┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-┃             ₳ĐłØ₴ ✦ ₳
-┣╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚄
-┃ 
-┃  ✎ *Usuario:* ${taguser}  
-┃  ✎ *Grupo:* ${groupMetadata.subject}  
-┃  ✎ *Miembros:* ${totalMembers}  
-┃  ✎ *Fecha:* ${date}  
-┃    
-┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-
-> *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
-> whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F`;
-
-      await conn.sendMessage(m.chat, { image: img, caption: despedida, mentions: [who] }, { quoted: fkontak1 });
-    }
-  }
-}*/
-
-
-
-/*import { WAMessageStubType } from '@whiskeysockets/baileys';
-import fetch from 'node-fetch';
-
-export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return true;
-
-  let totalMembers = participants.length;
-  let date = new Date().toLocaleString('es-ES', { timeZone: 'America/Mexico_City' });
-  let who = m.messageStubParameters[0];
-  let taguser = `@${who.split('@')[0]}`;
-  let chat = global.db.data.chats[m.chat];
-  let botname = global.botname || "Bot";
-
-  
-  const res1 = await fetch('https://files.catbox.moe/qhxt7c.png');
-  const img1 = Buffer.from(await res1.arrayBuffer());
-
-  const fkontak = {
-    key: { fromMe: false, participant: "0@s.whatsapp.net" },
-    message: {
-      productMessage: {
-        product: {
-          productImage: { jpegThumbnail: img1 },
-          title: `ʙɪᴇɴᴠᴇɴɪᴅᴏ, ᴀʜᴏʀᴀ sᴏᴍᴏs "${totalMembers}"`,
-          description: botname,
-          currencyCode: "USD",
-          priceAmount1000: "5000",
-          retailerId: "BOT"
-        },
-        businessOwnerJid: "0@s.whatsapp.net"
-      }
-    }
-  };
-
-  const res2 = await fetch('https://files.catbox.moe/8alfhv.png');
-  const img2 = Buffer.from(await res2.arrayBuffer());
-
-  const fkontak1 = {
-    key: { fromMe: false, participant: "0@s.whatsapp.net" },
-    message: {
-      productMessage: {
-        product: {
-          productImage: { jpegThumbnail: img2 },
-          title: `₳ĐłØ₴ Ʉ₴ɆⱤ ₳ⱧØⱤ₳ ₴Ø₥Ø₴ "${totalMembers}"`,
-          description: botname,
-          currencyCode: "USD",
-          priceAmount1000: "5000",
-          retailerId: "BOT"
-        },
-        businessOwnerJid: "0@s.whatsapp.net"
-      }
-    }
-  };
-
-  let imageUrl =  'https://files.catbox.moe/0183v7.png';
-
-  if (chat.welcome) {
-    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `
-┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-┃   ฿łɆ₦VɆ₦łĐØ ✦ ₳
-┣╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚄
-┃ 
-┃  ✎ *Usuario:* ${taguser}  
-┃  ✎ *Grupo:* ${groupMetadata.subject}  
-┃  ✎ *Miembros:* ${totalMembers}  
-┃  ✎ *Fecha:* ${date}  
-┃    
-┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-
-
-> *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
-> whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F`;
-      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: bienvenida, mentions: [who] }, { quoted: fkontak });
-    }
-
-    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE ||
-        m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
-      let despedida = `
-┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-┃             ₳ĐłØ₴ ✦ ₳
-┣╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚄
-┃ 
-┃  ✎ *Usuario:* ${taguser}  
-┃  ✎ *Grupo:* ${groupMetadata.subject}  
-┃  ✎ *Miembros:* ${totalMembers}  
-┃  ✎ *Fecha:* ${date}  
-┃    
-┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⚁
-
-
-> *sɪɢᴜᴇ ᴇʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ:*
-> whatsapp.com/channel/0029VbAzn9GGU3BQw830eA0F`;
-      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: despedida, mentions: [who] }, { quoted: fkontak1 });
-    }
-  }
-}*/
