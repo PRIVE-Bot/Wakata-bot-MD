b1420746792824b988e02960e4a9a736f10efc11
Deylin <deylinbo91@gmail.com>
2025-10-01 20:27:37 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -236,7 +236,7 @@ export async function handler(chatUpdate) {
 
                 const chatID = m.chat;
                 const ID_GRUPO_RESTRINGIDO = '120363421094353744@g.us';
-                const comandosPermitidos = ['code', 'qr', 'welcome', 'detect', 'kick', 'tag'];
+                const comandosPermitidos = ['code', 'qr', 'welcome', 'detect', 'kick', 'tag', 'antibot2'];
 
                 if (chatID === ID_GRUPO_RESTRINGIDO) {
                     const isComandoPermitido = comandosPermitidos.includes(command);
