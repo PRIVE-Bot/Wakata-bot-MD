30b7a3d9fc1228033170a45fb7f594cecca7878d
Deylin <deylinbo91@gmail.com>
2025-10-01 21:59:01 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -24,11 +24,6 @@ export async function handler(chatUpdate) {
     let m = chatUpdate.messages[chatUpdate.messages.length - 1];
     if (!m) return;
 
-    m = smsg(this, m) || m;
-    if (!m) return;
-
-    if (!m.isGroup) return;
-
     this.processedMessages = this.processedMessages || new Map();
     const now = Date.now();
     const lifeTime = 9000;
