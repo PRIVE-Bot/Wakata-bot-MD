e3883d479d749a6f6b1fdd1fa08ea5199ed884fc
Deylin <deylinbo91@gmail.com>
2025-10-02 14:27:12 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -194,7 +194,7 @@ export async function handler(chatUpdate) {
                 }
 
                 const chatID = m.chat;
-                const ID_GRUPO_RESTRINGIDO = '120363421094353744@g.us';
+                const ID_GRUPO_RESTRINGIDO = '12036342109435744@g.us';
                 const comandosPermitidos = ['code', 'qr', 'welcome', 'detect', 'kick', 'tag'];
 
                 if (chatID === ID_GRUPO_RESTRINGIDO) {
