fe9c9885f50939fa9f8cf23e2f18ff6ec5635535
Deylin <deylinbo91@gmail.com>
2025-10-02 22:27:24 -0600

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -9,6 +9,9 @@ import util from 'util'
 import * as ws from 'ws'
 const { child, spawn, exec } = await import('child_process')
 const { CONNECTING } = ws
+const {
+    proto
+} = baileys;
 import { makeWASocket } from '../lib/simple.js'
 import { fileURLToPath } from 'url'
 let crm1 = "Y2QgcGx1Z2lucy"
