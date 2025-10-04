ea255b14db16072eaac5ed526efd0c7675ef6c87
Deylin <deylinbo91@gmail.com>
2025-10-03 18:18:19 -0600

    Actualizar _welcome-message.js

--- plugins/_welcome-message.js
+++ plugins/_welcome-message.js
@@ -2,7 +2,7 @@ import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
 import fs from 'fs';
 
 const welcomeSent = {};
-const filePath = './estas/sent_welcome.json';
+const filePath = './extras/sent_welcome.json';
 
 if (fs.existsSync(filePath)) {
   Object.assign(welcomeSent, JSON.parse(fs.readFileSync(filePath, 'utf-8')));
