888a787dcbc3c04af5dc7ad2a7a7243a47c3596a
Deylin <deylinbo91@gmail.com>
2025-10-03 21:46:56 -0600

    Actualizar exec.js

--- plugins/owner/exec.js
+++ plugins/owner/exec.js
@@ -9,7 +9,6 @@ const require = createRequire(__dirname)
 
 let handler = async (m, _2) => {
   let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
-//if (!isOwner) return;
   let _return
   let _syntax = ''
   let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
@@ -19,12 +18,16 @@ let handler = async (m, _2) => {
     let f = {
       exports: {}
     }
-    let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
+    let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', 'p', _text)
     _return = await exec.call(conn, (...args) => {
       if (--i < 1) return
       console.log(...args)
       return conn.reply(m.chat, format(...args), m)
-    }, m, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
+    }, m, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2], (...args) => {
+      if (--i < 1) return
+      console.log(...args)
+      return conn.reply(m.chat, format(...args), m)
+    })
   } catch (e) {
     let err = syntaxerror(_text, 'Execution Function', {
       allowReturnOutsideFunction: true,
