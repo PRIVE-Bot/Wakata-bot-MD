922cc6048c254074fd62097058ce4f1c34b2261f
Deylin <deylinbo91@gmail.com>
2025-10-03 18:11:12 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import yargs from 'yargs';
 import { spawn, execSync } from 'child_process';
 import lodash from 'lodash';
 import { JadiBot } from './plugins/jadibot-serbot.js';
+import './plugins/main-allfake.js';
 import chalk from 'chalk';
 import syntaxerror from 'syntax-error';
 import { tmpdir } from 'os';
