855de3bcf609fc75b994ec809b08b3815c53ea30
Deylin <deylinbo91@gmail.com>
2025-10-14 21:42:57 -0600

    Actualizar kiss.js

--- plugins/anime/kiss.js
+++ plugins/anime/kiss.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+/*import fs from 'fs';
 import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
@@ -52,4 +52,4 @@ handler.tags = ['anime'];
 handler.command = ['kiss', 'beso'];
 handler.group = true;
 
-export default handler;
+export default handler;*/
