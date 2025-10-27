fe09df520b3fc06e027ed14de93019fe1147140c
Deylin <deylinbo91@gmail.com>
2025-10-27 00:53:34 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 import { smsg } from './lib/simple.js';
-import { format } from 'util';
+import { format } from 'util'; 
 import { fileURLToPath } from 'url';
 import path, { join } from 'path';
 import { unwatchFile, watchFile } from 'fs';
@@ -16,15 +16,24 @@ const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function (
 export async function handler(chatUpdate) {
     this.uptime = this.uptime || Date.now();
 
-    if (!chatUpdate || !chatUpdate.messages || chatUpdate.messages.length === 0) return;
+    if (!chatUpdate || !chatUpdate.messages || chatUpdate.messages.length === 0) {
+        return;
+    }
 
+        // aqui corrigo mi error
     let m = chatUpdate.messages[chatUpdate.messages.length - 1];
     if (!m) return;
+
+    m = smsg(this, m) || m;
+    if (!m) return;
+
+    if (!m.isGroup) return;
 
     this.processedMessages = this.processedMessages || new Map();
     const now = Date.now();
     const lifeTime = 9000;
 
+    // Limpiar mensajes procesados antiguos
     for (let [msgId, time] of this.processedMessages) {
         if (now - time > lifeTime) {
             this.processedMessages.delete(msgId);
@@ -135,21 +144,22 @@ export async function handler(chatUpdate) {
         if (opts['swonly'] && m.chat !== 'status@broadcast') return;
         if (typeof m.text !== 'string') m.text = '';
 
-        async function getLidFromJid(id, conn) {
+                async function getLidFromJid(id, conn) {
             if (id.endsWith('@lid')) return id;
             const res = await conn.onWhatsApp(id).catch(() => []);
             return res[0]?.lid || id;
         }
-        const senderLid = await getLidFromJid(m.sender, this); 
-        const botLid = await getLidFromJid(this.user.jid, this); 
-        const botJid = this.user.jid;
-        const groupMetadata = m.isGroup ? ((this.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {};
+        const senderLid = await getLidFromJid(m.sender, conn);
+        const botLid = await getLidFromJid(conn.user.jid, conn);
+        const botJid = conn.user.jid;
+        const groupMetadata = m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {};
         const participants = m.isGroup ? (groupMetadata.participants || []) : [];
         const user2 = participants.find(p => p.id === senderLid || p.jid === senderJid) || {};
         const bot = participants.find(p => p.id === botLid || p.id === botJid) || {};
         const isRAdmin = user2?.admin === "superadmin";
         const isAdmin = isRAdmin || user2?.admin === "admin";
         const isBotAdmin = !!bot?.admin;
+
 
         const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), './plugins');
         let usedPrefix = '';
